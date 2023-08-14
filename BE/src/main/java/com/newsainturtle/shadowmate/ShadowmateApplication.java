6995c047abed4770b47bd1fac5981be190f75281
yunaghgh <yunaghgh@naver.com>
2023-08-14 13:48:51 +0900

    [BE] create: 프로젝트 생성

---
+++ BE/src/main/java/com/newsainturtle/shadowmate/ShadowmateApplication.java
@@ -0,0 +1,13 @@
+package com.newsainturtle.shadowmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowmateApplication.class, args);
+	}
+
+}
