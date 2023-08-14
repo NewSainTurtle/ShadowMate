ac75b69e233397249c4f966e120fccbcbddec523
yunaghgh <yunaghgh@naver.com>
2023-08-14 17:57:05 +0900

    [BE] feat: 이메일 중복검사 컨트롤러 프로덕션 코드

--- BE/src/main/java/com/newsainturtle/shadowmate/auth/dto/CertifyEmailRequest.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/auth/dto/CertifyEmailRequest.java
@@ -3,11 +3,14 @@ package com.newsainturtle.shadowmate.auth.dto;
 import lombok.*;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class CertifyEmailRequest {
     @Email
+    @NotNull
     private String email;
 }
---
+++ BE/src/main/java/com/newsainturtle/shadowmate/common/GlobalExceptionHandler.java
@@ -0,0 +1,65 @@
+package com.newsainturtle.shadowmate.common;
+
+import com.newsainturtle.shadowmate.auth.exception.AuthErrorResult;
+import com.newsainturtle.shadowmate.auth.exception.AuthException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            final MethodArgumentNotValidException ex,
+            final HttpHeaders headers,
+            final HttpStatus status,
+            final WebRequest request) {
+
+        final List<String> errorList = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        log.warn("Invalid DTO Parameter errors : {}", errorList);
+        return this.makeErrorResponseEntity(errorList.toString());
+    }
+
+    private ResponseEntity<Object> makeErrorResponseEntity(final String errorDescription) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), errorDescription));
+    }
+
+    @ExceptionHandler({AuthException.class})
+    public ResponseEntity<ErrorResponse> handleAuthException(final AuthException exception) {
+        log.warn("AuthException occur: ", exception);
+        return this.makeAuthErrorResponseEntity(exception.getErrorResult());
+    }
+
+    private ResponseEntity<ErrorResponse> makeAuthErrorResponseEntity(final AuthErrorResult errorResult) {
+        return ResponseEntity.status(errorResult.getHttpStatus())
+                .body(new ErrorResponse(errorResult.name(), errorResult.getMessage()));
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    static class ErrorResponse {
+        private final String code;
+        private final String message;
+    }
+
+}
