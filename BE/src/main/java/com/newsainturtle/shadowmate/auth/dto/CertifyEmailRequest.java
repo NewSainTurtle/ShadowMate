9cf26ffc0d978459e7a3ae3ece1eb17c5b535ef7
tgb02087 <tgb02087@naver.com>
2023-08-14 17:57:41 +0900

    [BE] feat: 이메일 중복 검사 controller 개발

--- BE/src/main/java/com/newsainturtle/shadowmate/auth/dto/CertifyEmailRequest.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/auth/dto/CertifyEmailRequest.java
@@ -3,14 +3,18 @@ package com.newsainturtle.shadowmate.auth.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class CertifyEmailRequest {
 
     @Email
+    @NotNull
     private String email;
 }
