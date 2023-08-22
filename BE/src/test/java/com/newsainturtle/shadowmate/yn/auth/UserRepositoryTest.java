5fb653874dbd9272435fc2f97d19d2c3a8252090
yunaghgh <yunaghgh@naver.com>
2023-08-22 13:48:08 +0900

    [BE] test: 회원가입 레포지토리 테스트

--- BE/src/test/java/com/newsainturtle/shadowmate/yn/auth/UserRepositoryTest.java
+++ BE/src/test/java/com/newsainturtle/shadowmate/yn/auth/UserRepositoryTest.java
@@ -18,20 +18,21 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    final String email = "test1234@naver.com";
+    final User user = User.builder()
+            .email("test1234@naver.com")
+            .password("123456")
+            .socialLogin(false)
+            .nickname("거북이")
+            .plannerAccessScope(PlannerAccessScope.PUBLIC)
+            .withdrawal(false)
+            .build();
+
     @Nested
     class 이메일인증 {
         @Test
         public void 실패_이메일이_중복된_경우() {
             //given
-            final String email = "test1234@naver.com";
-            final User user = User.builder()
-                    .email("test1234@naver.com")
-                    .password("123456")
-                    .socialLogin(false)
-                    .nickname("거북이")
-                    .plannerAccessScope(PlannerAccessScope.PUBLIC)
-                    .withdrawal(false)
-                    .build();
             userRepository.save(user);
 
             //when
@@ -45,13 +46,33 @@ public class UserRepositoryTest {
         @Test
         public void 성공_이메일이_중복되지_않은_경우() {
             //given
-            final String email = "test1234@naver.com";
 
             //when
             final User result = userRepository.findByEmail(email);
 
             //then
             assertThat(result).isNull();
+        }
+    }
+
+    @Nested
+    class 회원가입{
+        @Test
+        public void 사용자등록(){
+            //given
+            userRepository.save(user);
+
+            //when
+            final User result = userRepository.findByEmail(user.getEmail());
+
+            //then
+            assertThat(result).isNotNull();
+            assertThat(result.getEmail()).isEqualTo(user.getEmail());
+            assertThat(result.getPassword()).isEqualTo(user.getPassword());
+            assertThat(result.getSocialLogin()).isEqualTo(user.getSocialLogin());
+            assertThat(result.getNickname()).isEqualTo(user.getNickname());
+            assertThat(result.getPlannerAccessScope()).isEqualTo(user.getPlannerAccessScope());
+            assertThat(result.getWithdrawal()).isEqualTo(user.getWithdrawal());
         }
     }
 
