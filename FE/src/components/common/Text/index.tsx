5025f0a1fd9da23685c6574dbbcad002ac284a19
jhkang9820 <jhkang9820@gmail.com>
2023-08-22 16:09:00 +0900

    [FE] feat: 텍스트 컴포넌트 생성

--- FE/src/components/common/Text/index.tsx
+++ FE/src/components/common/Text/index.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styles from "@styles/common/Text.module.scss";
+import colors, { ColorsKeyTypes } from "src/util/colors";
 
-const Text = () => {
-  return <></>;
+interface Props {
+  value: string;
+  type?: "smallText" | "caption" | "text" | "groupTitle" | "textTitle" | "pageTitle" | "title";
+  bold?: boolean;
+  color?: ColorsKeyTypes;
+  en?: boolean;
+}
+
+const Text = ({ value, type = "text", bold, color = "colorBlack", en }: Props) => {
+  console.log(color);
+  const style = {
+    fontFamily: en ? "Montserrat" : "Pretendard-Regular",
+    fontWeight: bold ? "600" : "400",
+    color: colors[color],
+  };
+
+  return (
+    <>
+      <span className={`${styles[type]}`} style={style}>
+        {value}
+      </span>
+    </>
+  );
+};
+
+Text.defaultProps = {
+  value: "내용이 들어갑니다.",
+  type: "text",
+  color: "black",
 };
 
 export default Text;
