9a01f494ad7505b2cbe55aad29aaae0aee75095f
jhkang9820 <jhkang9820@gmail.com>
2023-08-28 17:01:36 +0900

    [FE] feat: 버튼 컴포넌트 생성

--- FE/src/components/common/Button/index.tsx
+++ FE/src/components/common/Button/index.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { DOMAttributes, ReactNode } from "react";
+import styles from "@styles/common/Button.module.scss";
+import Text from "../Text";
 
-const Button = () => {
-  return <></>;
+interface ButtonProps {
+  children: ReactNode;
+  type?: "gray" | "red" | "blue";
+  onClick?: DOMAttributes<HTMLDivElement>;
+}
+
+const Button = ({ children, type = "blue", onClick }: ButtonProps) => {
+  return (
+    <div className={`${styles.button} ${styles[`__${type}`]}`}>
+      <Text type="small">{children}</Text>
+    </div>
+  );
 };
 
 export default Button;
--- FE/src/pages/commonPage.tsx
+++ FE/src/pages/commonPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Text from "@components/common/Text";
+import Button from "@components/common/Button";
 
 const commonPage = () => {
   return (
@@ -16,6 +17,12 @@ const commonPage = () => {
         <Text type="semi-large">프리텐다드 - semi-large </Text>
         <br /> <br />
         <Text type="large">프리텐다드 - large</Text>
+      </div>
+      <div>
+        <Button type="blue">소설공유</Button>
+        <Button children="♥ 50" type="red" />
+        <Button children="일간 보기" type="gray" />
+        <Button children="로그아웃" type="gray" />
       </div>
     </>
   );
