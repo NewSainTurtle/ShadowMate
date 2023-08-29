07466999ccea7de86c02a2adcce5555ba6727b62
dustnzlzl <dustnzlzl@naver.com>
2023-08-30 02:09:55 +0900

    [FE] feat: mui 기본 css 세팅

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./App.scss";
 import Header from "@components/common/Header";
 import MainPage from "@pages/MainPage";
 import LoginPage from "@pages/LoginPage";
 import CommonPage from "@pages/commonPage";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Pretendard-Regular",
+  },
+});
 
 const App = () => {
   useEffect(() => {
@@ -11,14 +19,14 @@ const App = () => {
   }, []);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <Routes>
         <Route path="/common" element={<CommonPage />} />
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
-    </>
+    </ThemeProvider>
   );
 };
 export default App;
--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.scss";
 import { BrowserRouter } from "react-router-dom";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <BrowserRouter>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
   </BrowserRouter>,
 );
