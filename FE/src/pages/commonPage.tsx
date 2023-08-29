c69908ac57bd54d26c76557902dcb7df86b2d274
dustnzlzl <dustnzlzl@naver.com>
2023-08-29 16:05:39 +0900

    [FE] feat: Dday 공통 컴포넌트 생성

--- FE/src/pages/commonPage.tsx
+++ FE/src/pages/commonPage.tsx
@@ -1,28 +1,33 @@
 import React, { useState } from "react";
 import Text from "@components/common/Text";
 import Button from "@components/common/Button";
+import Dday from "@components/common/Dday";
+import Input from "@components/common/Input";
 
 const commonPage = () => {
   return (
     <>
       <div>
-        <Text type="small">프리텐다드 - small</Text>
+        <Text types="small">프리텐다드 - small</Text>
         <br /> <br />
-        <Text type="default">프리텐다드 - default</Text>
+        <Text types="default">프리텐다드 - default</Text>
         <br /> <br />
-        <Text type="semi-medium">프리텐다드 - semi-medium</Text>
+        <Text types="semi-medium">프리텐다드 - semi-medium</Text>
         <br /> <br />
-        <Text type="medium">프리텐다드 - medium</Text>
+        <Text types="medium">프리텐다드 - medium</Text>
         <br /> <br />
-        <Text type="semi-large">프리텐다드 - semi-large </Text>
+        <Text types="semi-large">프리텐다드 - semi-large </Text>
         <br /> <br />
-        <Text type="large">프리텐다드 - large</Text>
+        <Text types="large">프리텐다드 - large</Text>
       </div>
       <div>
-        <Button type="blue">소설공유</Button>
-        <Button children="♥ 50" type="red" />
-        <Button children="일간 보기" type="gray" />
-        <Button children="로그아웃" type="gray" />
+        <Button types="blue">소설공유</Button>
+        <Button children="♥ 50" types="red" />
+        <Button children="일간 보기" types="gray" />
+        <Button children="로그아웃" types="gray" />
+      </div>
+      <div>
+        <Dday>-127</Dday>
       </div>
     </>
   );
