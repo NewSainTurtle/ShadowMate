450a77798575338be7b84e15a2df0da95790329b
tgb02087 <tgb02087@naver.com>
2023-08-29 13:54:02 +0900

    [BE] feat: authenticationManager 빈 추가

--- BE/src/main/java/com/newsainturtle/shadowmate/config/AppConfig.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/config/AppConfig.java
@@ -2,6 +2,8 @@ package com.newsainturtle.shadowmate.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -9,5 +11,11 @@ public class AppConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
