70234aa428da69976d0a74c9f433f576f381e35c
yunaghgh <yunaghgh@naver.com>
2023-08-31 16:07:04 +0900

    [BE] feat: 플래너 설정 예외처리 클래스 생성

---
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/exception/PlannerSettingErrorResult.java
@@ -0,0 +1,16 @@
+package com.newsainturtle.shadowmate.planner_setting.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PlannerSettingErrorResult {
+    UNREGISTERED_USER(HttpStatus.BAD_REQUEST, "등록되지 않은 사용자입니다."),
+    INVALID_CATEGORY_COLOR(HttpStatus.BAD_REQUEST, "카테고리 색상 ID가 유효하지 않습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
