ba8366c3b13bc0b4d5203e805bb327ebc85f0d0e
yunaghgh <yunaghgh@naver.com>
2023-08-31 18:05:14 +0900

    [BE] feat: 카테고리 등록 컨트롤러 프로덕션 코드 작성

---
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/constant/PlannerSettingConstant.java
@@ -0,0 +1,9 @@
+package com.newsainturtle.shadowmate.planner_setting.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PlannerSettingConstant {
+    public static final String SUCCESS_ADD_CATEGORY = "플래너 카테고리 등록이 완료되었습니다.";
+}
