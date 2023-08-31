ee39e89be3177b877349994c74bfe9067d4a62b4
jhkang9820 <jhkang9820@gmail.com>
2023-08-31 16:13:46 +0900

    [FE] feat: 페이지 생성 및 router 연결

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -5,7 +5,14 @@ import "./App.scss";
 import Header from "@components/common/Header";
 import MainPage from "@pages/MainPage";
 import LoginPage from "@pages/LoginPage";
+import SignupPage from "@pages/SignupPage";
+import DayPage from "@pages/Planner/DayPage";
+import MonthPage from "@pages/Planner/MonthPage";
+import WeekPage from "@pages/Planner/WeekPage";
+import SocialPage from "@pages/SocialPage";
 import CommonPage from "@pages/commonPage";
+import NotFoundPage from "@pages/NotFoundPage";
+import MyPage from "@pages/Mypage";
 
 const theme = createTheme({
   typography: {
@@ -21,11 +28,25 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      <Routes>
-        <Route path="/common" element={<CommonPage />} />
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+      <div id="App" style={{ marginLeft: "10em" }}>
+        <Routes>
+          <Route path="/common" element={<CommonPage />} />
+          
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          
+          <Route path="/day" element={<DayPage />} />
+          <Route path="/week" element={<WeekPage />} />
+          <Route path="/month" element={<MonthPage />} />
+          
+          <Route path="/social" element={<SocialPage />} />
+
+          <Route path="/mypage" element={<MyPage />} />
+          
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </ThemeProvider>
   );
 };
---
+++ FE/src/pages/SignupPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignupPage = () => {
+  return <span>SignupPage</span>;
+};
+
+export default SignupPage;
