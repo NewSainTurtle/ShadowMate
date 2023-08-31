088b2b4a6cf4a4cccb05ed40ea1af3aa3b883e93
yunaghgh <yunaghgh@naver.com>
2023-08-31 16:09:21 +0900

    [BE] rename: 플래너 설정 관련 서비스 테스트 클래스명 변경

--- BE/src/test/java/com/newsainturtle/shadowmate/yn/planner_setting/PlannerSettingServiceTest.java
+++ BE/src/test/java/com/newsainturtle/shadowmate/yn/planner_setting/PlannerSettingServiceTest.java
@@ -26,7 +26,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class CategoryServiceTest {
+class PlannerSettingServiceTest {
 
     @InjectMocks
     PlannerSettingServiceImpl plannerSettingService;
