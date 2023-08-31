473334dda57f9cd2f29cea8ff18c667f34c268e3
yunaghgh <yunaghgh@naver.com>
2023-08-31 16:04:51 +0900

    [BE] test: 카테고리 등록 서비스 테스트코드 작성

---
+++ BE/src/test/java/com/newsainturtle/shadowmate/yn/planner_setting/CategoryServiceTest.java
@@ -0,0 +1,141 @@
+package com.newsainturtle.shadowmate.yn.planner_setting;
+
+import com.newsainturtle.shadowmate.planner_setting.dto.AddCategoryRequest;
+import com.newsainturtle.shadowmate.planner_setting.entity.Category;
+import com.newsainturtle.shadowmate.planner_setting.entity.CategoryColor;
+import com.newsainturtle.shadowmate.planner_setting.exception.PlannerSettingErrorResult;
+import com.newsainturtle.shadowmate.planner_setting.exception.PlannerSettingException;
+import com.newsainturtle.shadowmate.planner_setting.repository.CategoryColorRepository;
+import com.newsainturtle.shadowmate.planner_setting.repository.CategoryRepository;
+import com.newsainturtle.shadowmate.planner_setting.service.PlannerSettingServiceImpl;
+import com.newsainturtle.shadowmate.user.entity.User;
+import com.newsainturtle.shadowmate.user.enums.PlannerAccessScope;
+import com.newsainturtle.shadowmate.user.repository.UserRepository;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+
+    @InjectMocks
+    PlannerSettingServiceImpl plannerSettingService;
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @Mock
+    CategoryColorRepository categoryColorRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+
+    @Nested
+    class 카테고리등록 {
+        final User user = User.builder()
+                .email("test@test.com")
+                .password("123456")
+                .socialLogin(false)
+                .nickname("거북이")
+                .plannerAccessScope(PlannerAccessScope.PUBLIC)
+                .withdrawal(false)
+                .build();
+        final CategoryColor categoryColor = CategoryColor.builder()
+                .categoryColorCode("D9B5D9")
+                .build();
+        final Long userId = 1L;
+        final AddCategoryRequest request = AddCategoryRequest.builder()
+                .categoryTitle("국어")
+                .categoryEmoticon("🍅")
+                .categoryColorId(1L)
+                .build();
+        final Category category = Category.builder()
+                .categoryTitle(request.getCategoryTitle())
+                .categoryEmoticon(request.getCategoryEmoticon())
+                .categoryRemove(false)
+                .categoryColor(categoryColor)
+                .user(user)
+                .build();
+
+        @Test
+        public void 실패_없는사용자() {
+            //given
+            doReturn(Optional.empty()).when(userRepository).findById(userId);
+
+            //when
+            final PlannerSettingException result = assertThrows(PlannerSettingException.class, () -> plannerSettingService.addCategory(userId, request));
+
+            //then
+            assertThat(result.getErrorResult()).isEqualTo(PlannerSettingErrorResult.UNREGISTERED_USER);
+        }
+
+        @Test
+        public void 실패_없는카테고리색상() {
+            //given
+
+            doReturn(Optional.of(user)).when(userRepository).findById(userId);
+            doReturn(Optional.empty()).when(categoryColorRepository).findById(request.getCategoryColorId());
+
+            //when
+            final PlannerSettingException result = assertThrows(PlannerSettingException.class, () -> plannerSettingService.addCategory(userId, request));
+
+            //then
+            assertThat(result.getErrorResult()).isEqualTo(PlannerSettingErrorResult.INVALID_CATEGORY_COLOR);
+        }
+
+        @Test
+        public void 성공_카테고리등록_이모티콘없음() {
+            //given
+            final AddCategoryRequest addCategoryRequest = AddCategoryRequest.builder()
+                    .categoryTitle("국어")
+                    .categoryEmoticon(null)
+                    .categoryColorId(1L)
+                    .build();
+
+            doReturn(Optional.of(user)).when(userRepository).findById(userId);
+            doReturn(Optional.of(categoryColor)).when(categoryColorRepository).findById(request.getCategoryColorId());
+            doReturn(category).when(categoryRepository).save(any(Category.class));
+
+            //when
+            plannerSettingService.addCategory(userId, addCategoryRequest);
+
+            //then
+
+            //verify
+            verify(userRepository, times(1)).findById(userId);
+            verify(categoryColorRepository, times(1)).findById(request.getCategoryColorId());
+            verify(categoryRepository, times(1)).save(any(Category.class));
+        }
+
+        @Test
+        public void 성공_카테고리등록_이모티콘있음() {
+            //given
+            doReturn(Optional.of(user)).when(userRepository).findById(userId);
+            doReturn(Optional.of(categoryColor)).when(categoryColorRepository).findById(request.getCategoryColorId());
+            doReturn(category).when(categoryRepository).save(any(Category.class));
+
+            //when
+            plannerSettingService.addCategory(userId, request);
+
+            //then
+
+            //verify
+            verify(userRepository, times(1)).findById(userId);
+            verify(categoryColorRepository, times(1)).findById(request.getCategoryColorId());
+            verify(categoryRepository, times(1)).save(any(Category.class));
+        }
+    }
+
+
+}
