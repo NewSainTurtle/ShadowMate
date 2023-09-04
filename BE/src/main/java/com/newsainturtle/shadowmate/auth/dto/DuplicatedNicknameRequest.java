afe03a4fcf6ae20d7b9c51ed2175306cbb095bd2
tgb02087 <tgb02087@naver.com>
2023-09-04 14:18:41 +0900

    [BE] refactor: builder패턴 추가, 길이제한 추가

--- BE/src/main/java/com/newsainturtle/shadowmate/auth/dto/DuplicatedNicknameRequest.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/auth/dto/DuplicatedNicknameRequest.java
@@ -1,18 +1,23 @@
 package com.newsainturtle.shadowmate.auth.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class DuplicatedNicknameRequest {
 
     @NotNull
+    @Size(min = 2, max = 10)
     private String nickname;
+
+    @Builder
+    public DuplicatedNicknameRequest(String nickname) {
+        this.nickname = nickname;
+    }
 }
