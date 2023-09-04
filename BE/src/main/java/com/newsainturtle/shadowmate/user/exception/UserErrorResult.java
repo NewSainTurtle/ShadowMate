74374ef68d986d1fe142156b76bbe88d64d425d0
tgb02087 <tgb02087@naver.com>
2023-09-04 22:02:50 +0900

    [BE] feat: userException 생성

---
+++ BE/src/main/java/com/newsainturtle/shadowmate/user/exception/UserErrorResult.java
@@ -0,0 +1,18 @@
+package com.newsainturtle.shadowmate.user.exception;
+
+import com.newsainturtle.shadowmate.common.BaseErrorResult;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorResult implements BaseErrorResult {
+
+    NOT_FOUND_PROFILE(HttpStatus.NOT_FOUND, "프로필을 찾을 수 없습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
