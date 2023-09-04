7f7f59f1e5dacc19f371f3b76666eab0fcaf22d7
yunaghgh <yunaghgh@naver.com>
2023-09-04 15:41:40 +0900

    [BE] feat: 카테고리 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/CategoryRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/CategoryRepository.java
@@ -1,10 +1,13 @@
 package com.newsainturtle.shadowmate.planner_setting.repository;
 
 import com.newsainturtle.shadowmate.planner_setting.entity.Category;
+import com.newsainturtle.shadowmate.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+    List<Category> findByUser(final User user);
 }
