9871a92cecc3b59268bf291c9fcd2e0f64104e0b
yunaghgh <yunaghgh@naver.com>
2023-09-05 14:12:33 +0900

    [BE] feat: 사용자 인증 서비스 예외처리 메시지

--- BE/src/main/java/com/newsainturtle/shadowmate/auth/exception/AuthErrorResult.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/auth/exception/AuthErrorResult.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorResult {
+    UNREGISTERED_USER(HttpStatus.BAD_REQUEST, "등록되지 않은 사용자입니다."),
     DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST,"중복된 이메일입니다."),
     DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "중복된 닉네임입니다."),
     FAIL_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR,"이메일 전송에 실패했습니다."),
