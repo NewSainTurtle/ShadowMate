be3f04996ab5a247205c98ef2f43b0fabcdf01bf
dustnzlzl <dustnzlzl@naver.com>
2023-09-13 06:46:08 +0900

    [FE] test: 월별 이동 임시 플래너 추가

--- FE/src/pages/Planner/MonthPage.tsx
+++ FE/src/pages/Planner/MonthPage.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const MonthPage = () => {
-  return <span>MonthPage</span>;
+  const [day, setDay] = useState("");
+
+  return (
+    <>
+      <span>MonthPage</span>
+      <p>
+        <input
+          type="date"
+          value={day}
+          onChange={(e) => {
+            setDay(e.target.value);
+          }}
+        />
+        <NavLink to={"/day"} state={{ date: day }}>
+          이동
+        </NavLink>
+      </p>
+    </>
+  );
 };
 
 export default MonthPage;
