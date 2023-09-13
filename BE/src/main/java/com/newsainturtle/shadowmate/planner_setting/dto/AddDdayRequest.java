eb106cd7e5dad761d6c160aa2281852f03474fe4
yunaghgh <yunaghgh@naver.com>
2023-09-13 16:28:39 +0900

    [BE] modify: 디데이 등록 dto 날짜 타입 수정

--- BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/dto/AddDdayRequest.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/dto/AddDdayRequest.java
@@ -1,14 +1,13 @@
 package com.newsainturtle.shadowmate.planner_setting.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import java.sql.Date;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,11 +17,12 @@ public class AddDdayRequest {
     @Size(max = 40)
     private String ddayTitle;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-    private Date ddayDate;
+    @NotNull
+    @Pattern(regexp = "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$")
+    private String ddayDate;
 
     @Builder
-    public AddDdayRequest(String ddayTitle, Date ddayDate) {
+    public AddDdayRequest(String ddayTitle, String ddayDate) {
         this.ddayTitle = ddayTitle;
         this.ddayDate = ddayDate;
     }
