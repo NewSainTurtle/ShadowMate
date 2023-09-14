a4d70e133333665eb2d6030ff7cc038af8481bb2
yunaghgh <yunaghgh@naver.com>
2023-09-14 13:29:16 +0900

    [BE] feat: 디데이 목록 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/DdayRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/DdayRepository.java
@@ -1,9 +1,13 @@
 package com.newsainturtle.shadowmate.planner_setting.repository;
 
 import com.newsainturtle.shadowmate.planner_setting.entity.Dday;
+import com.newsainturtle.shadowmate.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DdayRepository extends JpaRepository<Dday, Long> {
+    List<Dday> findByUserOrderByDdayDateDesc(final User user);
 }
