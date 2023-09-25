cb855e57d7ce590842e727a8368cfb2a93fa2cd2
yunaghgh <yunaghgh@naver.com>
2023-09-25 14:46:15 +0900

    [BE] test: 일일플래너 등록 레포지토리 테스트 코드 작성

---
+++ BE/src/test/java/com/newsainturtle/shadowmate/yn/planner/repository/DailyPlannerRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.newsainturtle.shadowmate.yn.planner.repository;
+
+import com.newsainturtle.shadowmate.planner.entity.DailyPlanner;
+import com.newsainturtle.shadowmate.planner.repository.DailyPlannerRepository;
+import com.newsainturtle.shadowmate.user.entity.User;
+import com.newsainturtle.shadowmate.user.enums.PlannerAccessScope;
+import com.newsainturtle.shadowmate.user.enums.SocialType;
+import com.newsainturtle.shadowmate.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.sql.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DailyPlannerRepositoryTest {
+
+    @Autowired
+    DailyPlannerRepository dailyPlannerRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+
+    @BeforeEach
+    public void init() {
+        user = userRepository.save(User.builder()
+                .email("test1234@naver.com")
+                .password("123456")
+                .socialLogin(SocialType.BASIC)
+                .nickname("거북이")
+                .plannerAccessScope(PlannerAccessScope.PUBLIC)
+                .withdrawal(false)
+                .build());
+    }
+
+    @Test
+    public void 일일플래너등록() {
+        //given
+        final DailyPlanner dailyPlanner = DailyPlanner.builder()
+                .dailyPlannerDay(Date.valueOf("2023-09-25"))
+                .user(user)
+                .build();
+
+        //when
+        final DailyPlanner saveDailyPlanner = dailyPlannerRepository.save(dailyPlanner);
+
+        //then
+        assertThat(saveDailyPlanner).isNotNull();
+        assertThat(saveDailyPlanner.getDailyPlannerDay()).isEqualTo(Date.valueOf("2023-09-25"));
+        assertThat(saveDailyPlanner.getUser()).isEqualTo(user);
+        assertThat(saveDailyPlanner.getRetrospection()).isNull();
+        assertThat(saveDailyPlanner.getRetrospectionImage()).isNull();
+        assertThat(saveDailyPlanner.getTodayGoal()).isNull();
+        assertThat(saveDailyPlanner.getTomorrowGoal()).isNull();
+    }
+}
