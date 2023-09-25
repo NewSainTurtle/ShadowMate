98920b34d68ad23e30346e62e7ee882c1baa7bc6
yunaghgh <yunaghgh@naver.com>
2023-09-25 16:55:23 +0900

    [BE] feat: 일일플래너 등록 컨트롤러 프로덕션 코드 작성

---
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/constant/PlannerConstant.java
@@ -0,0 +1,9 @@
+package com.newsainturtle.shadowmate.planner.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PlannerConstant {
+    public static final String SUCCESS_ADD_DAILY_TODO = "일일플래너 할일 등록이 완료되었습니다.";
+}
