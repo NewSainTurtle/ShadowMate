d5eb730946c8c1e972de7b45e7db1f0bcbee01ad
yunaghgh <yunaghgh@naver.com>
2023-09-25 16:55:23 +0900

    [BE] feat: 일일플래너 등록 컨트롤러 프로덕션 코드 작성

---
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/controller/PlannerController.java
@@ -0,0 +1,33 @@
+package com.newsainturtle.shadowmate.planner.controller;
+
+import com.newsainturtle.shadowmate.auth.service.AuthService;
+import com.newsainturtle.shadowmate.common.BaseResponse;
+import com.newsainturtle.shadowmate.config.auth.PrincipalDetails;
+import com.newsainturtle.shadowmate.planner.dto.AddDailyTodoRequest;
+import com.newsainturtle.shadowmate.planner.service.DailyPlannerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static com.newsainturtle.shadowmate.planner.constant.PlannerConstant.SUCCESS_ADD_DAILY_TODO;
+
+@RestController
+@RequestMapping("/api/planners")
+@RequiredArgsConstructor
+public class PlannerController {
+
+    private final DailyPlannerService dailyPlannerServiceImpl;
+    private final AuthService authServiceImpl;
+
+    @PostMapping("/{userId}/daily/todos")
+    public ResponseEntity<BaseResponse> addDailyTodo(@AuthenticationPrincipal final PrincipalDetails principalDetails,
+                                                     @PathVariable("userId") final Long userId,
+                                                     @RequestBody @Valid final AddDailyTodoRequest addDailyTodoRequest) {
+        authServiceImpl.certifyUser(userId, principalDetails.getUser());
+        return ResponseEntity.ok(BaseResponse.from(SUCCESS_ADD_DAILY_TODO, dailyPlannerServiceImpl.addDailyTodo(principalDetails.getUser(), addDailyTodoRequest)));
+    }
+
+}
