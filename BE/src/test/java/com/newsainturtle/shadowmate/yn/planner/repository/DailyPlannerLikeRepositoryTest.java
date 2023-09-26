a30187cd7e75ca1ff638f0c8853ed6b11abb5a12
yunaghgh <yunaghgh@naver.com>
2023-09-26 15:50:06 +0900

    [BE] test: 일일플래너 좋아요 등록 레포지토리 테스트 코드 작성

---
+++ BE/src/test/java/com/newsainturtle/shadowmate/yn/planner/repository/DailyPlannerLikeRepositoryTest.java
@@ -0,0 +1,78 @@
+package com.newsainturtle.shadowmate.yn.planner.repository;
+
+import com.newsainturtle.shadowmate.planner.entity.DailyPlanner;
+import com.newsainturtle.shadowmate.planner.entity.DailyPlannerLike;
+import com.newsainturtle.shadowmate.planner.repository.DailyPlannerLikeRepository;
+import com.newsainturtle.shadowmate.planner.repository.DailyPlannerRepository;
+import com.newsainturtle.shadowmate.user.entity.User;
+import com.newsainturtle.shadowmate.user.enums.PlannerAccessScope;
+import com.newsainturtle.shadowmate.user.enums.SocialType;
+import com.newsainturtle.shadowmate.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.sql.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DailyPlannerLikeRepositoryTest {
+
+    @Autowired
+    private DailyPlannerLikeRepository dailyPlannerLikeRepository;
+
+    @Autowired
+    private DailyPlannerRepository dailyPlannerRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user1;
+    private User user2;
+
+    @BeforeEach
+    public void init() {
+        user1 = userRepository.save(User.builder()
+                .email("test1234@naver.com")
+                .password("123456")
+                .socialLogin(SocialType.BASIC)
+                .nickname("거북이")
+                .plannerAccessScope(PlannerAccessScope.PUBLIC)
+                .withdrawal(false)
+                .build());
+        user2 = userRepository.save(User.builder()
+                .email("test127@naver.com")
+                .password("123456")
+                .socialLogin(SocialType.BASIC)
+                .nickname("토끼")
+                .plannerAccessScope(PlannerAccessScope.PUBLIC)
+                .withdrawal(false)
+                .build());
+    }
+
+    @Test
+    public void 좋아요등록() {
+        //given
+        final DailyPlanner saveDailyPlanner = dailyPlannerRepository.save(DailyPlanner.builder()
+                .dailyPlannerDay(Date.valueOf("2023-09-25"))
+                .user(user1)
+                .build());
+
+        //when
+        final DailyPlannerLike saveDailyPlannerLike = dailyPlannerLikeRepository.save(DailyPlannerLike.builder()
+                .dailyPlanner(saveDailyPlanner)
+                .user(user2)
+                .build());
+
+        //then
+        assertThat(saveDailyPlannerLike).isNotNull();
+        assertThat(saveDailyPlannerLike.getDailyPlanner()).isEqualTo(saveDailyPlanner);
+        assertThat(saveDailyPlannerLike.getUser()).isEqualTo(user2);
+    }
+
+}
