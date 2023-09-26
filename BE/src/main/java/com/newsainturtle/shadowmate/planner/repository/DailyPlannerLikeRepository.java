a982e6d2c35bf3bf3cc146fa81865eba2db016df
yunaghgh <yunaghgh@naver.com>
2023-09-26 16:24:44 +0900

    [BE] feat: 일일플래너 사용자 아이디로 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/DailyPlannerLikeRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/DailyPlannerLikeRepository.java
@@ -2,10 +2,11 @@ package com.newsainturtle.shadowmate.planner.repository;
 
 import com.newsainturtle.shadowmate.planner.entity.DailyPlanner;
 import com.newsainturtle.shadowmate.planner.entity.DailyPlannerLike;
+import com.newsainturtle.shadowmate.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DailyPlannerLikeRepository extends JpaRepository<DailyPlannerLike, Long> {
-    DailyPlannerLike findByUserIdAndDailyPlanner(final Long userId, final DailyPlanner dailyPlanner);
+    DailyPlannerLike findByUserAndDailyPlanner(final User user, final DailyPlanner dailyPlanner);
 }
