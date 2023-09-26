854d16fb0c4a2a48a74af16fd5513e42db057718
yunaghgh <yunaghgh@naver.com>
2023-09-26 14:23:44 +0900

    [BE] feat: 오늘의 다짐 편집 서비스 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/service/DailyPlannerServiceImpl.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/service/DailyPlannerServiceImpl.java
@@ -2,6 +2,7 @@ package com.newsainturtle.shadowmate.planner.service;
 
 import com.newsainturtle.shadowmate.planner.dto.AddDailyTodoRequest;
 import com.newsainturtle.shadowmate.planner.dto.AddDailyTodoResponse;
+import com.newsainturtle.shadowmate.planner.dto.UpdateTodayGoalRequest;
 import com.newsainturtle.shadowmate.planner.entity.DailyPlanner;
 import com.newsainturtle.shadowmate.planner.entity.Todo;
 import com.newsainturtle.shadowmate.planner.enums.TodoStatus;
@@ -60,5 +61,22 @@ public class DailyPlannerServiceImpl implements DailyPlannerService {
                 .build();
         final Todo saveTodo = todoRepository.save(todo);
         return AddDailyTodoResponse.builder().todoId(saveTodo.getId()).build();
+    }
+
+    @Override
+    @Transactional
+    public void updateTodayGoal(final User user, final UpdateTodayGoalRequest updateTodayGoalRequest) {
+        final DailyPlanner dailyPlanner = getDailyPlanner(user, updateTodayGoalRequest.getDate());
+        final DailyPlanner changeDailyPlanner = DailyPlanner.builder()
+                .id(dailyPlanner.getId())
+                .createTime(dailyPlanner.getCreateTime())
+                .dailyPlannerDay(dailyPlanner.getDailyPlannerDay())
+                .user(dailyPlanner.getUser())
+                .retrospection(dailyPlanner.getRetrospection())
+                .retrospectionImage(dailyPlanner.getRetrospectionImage())
+                .tomorrowGoal(dailyPlanner.getTomorrowGoal())
+                .todayGoal(updateTodayGoalRequest.getTodayGoal())
+                .build();
+        dailyPlannerRepository.save(changeDailyPlanner);
     }
 }
