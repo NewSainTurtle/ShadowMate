d75cc3af24e019fd689134049b6dac3cf44864d9
yunaghgh <yunaghgh@naver.com>
2023-09-26 16:08:27 +0900

    [BE] modify: 일일플래너 좋아요 조회 파라미터 수정

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/DailyPlannerLikeRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/DailyPlannerLikeRepository.java
@@ -2,11 +2,10 @@ package com.newsainturtle.shadowmate.planner.repository;
 
 import com.newsainturtle.shadowmate.planner.entity.DailyPlanner;
 import com.newsainturtle.shadowmate.planner.entity.DailyPlannerLike;
-import com.newsainturtle.shadowmate.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DailyPlannerLikeRepository extends JpaRepository<DailyPlannerLike, Long> {
-    DailyPlannerLike findByUserAndDailyPlanner(final User user, final DailyPlanner dailyPlanner);
+    DailyPlannerLike findByUserIdAndDailyPlanner(final Long userId, final DailyPlanner dailyPlanner);
 }
