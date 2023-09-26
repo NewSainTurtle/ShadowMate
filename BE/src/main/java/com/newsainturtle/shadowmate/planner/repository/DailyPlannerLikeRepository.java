dfb14e6e76bee981bbbd80aec1001506836397c1
yunaghgh <yunaghgh@naver.com>
2023-09-26 15:50:22 +0900

    [BE] feat: 일일플래너 좋아요 레포지토리 생성

---
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/DailyPlannerLikeRepository.java
@@ -0,0 +1,11 @@
+package com.newsainturtle.shadowmate.planner.repository;
+
+import com.newsainturtle.shadowmate.planner.entity.DailyPlanner;
+import com.newsainturtle.shadowmate.planner.entity.DailyPlannerLike;
+import com.newsainturtle.shadowmate.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DailyPlannerLikeRepository extends JpaRepository<DailyPlannerLike, Long> {
+}
