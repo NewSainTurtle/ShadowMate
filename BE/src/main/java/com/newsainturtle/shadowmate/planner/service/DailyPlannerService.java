d74d703d4b9eb7747a36292e72110ac5cd052c76
yunaghgh <yunaghgh@naver.com>
2023-09-26 14:46:31 +0900

    [BE] feat: 내일의 다짐 편집 서비스 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/service/DailyPlannerService.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/service/DailyPlannerService.java
@@ -3,9 +3,11 @@ package com.newsainturtle.shadowmate.planner.service;
 import com.newsainturtle.shadowmate.planner.dto.AddDailyTodoRequest;
 import com.newsainturtle.shadowmate.planner.dto.AddDailyTodoResponse;
 import com.newsainturtle.shadowmate.planner.dto.UpdateTodayGoalRequest;
+import com.newsainturtle.shadowmate.planner.dto.UpdateTomorrowGoalRequest;
 import com.newsainturtle.shadowmate.user.entity.User;
 
 public interface DailyPlannerService {
     AddDailyTodoResponse addDailyTodo(final User user, final AddDailyTodoRequest addDailyTodoRequest);
     void updateTodayGoal(final User user, final UpdateTodayGoalRequest updateTodayGoalRequest);
+    void updateTomorrowGoal(final User user, final UpdateTomorrowGoalRequest updateTomorrowGoalRequest);
 }
