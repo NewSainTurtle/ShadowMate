4f223c40a13f3dad2dbfe41d4cda4223809853e0
yunaghgh <yunaghgh@naver.com>
2023-09-26 14:32:31 +0900

    [BE] feat: 오늘의 다짐 편집 컨트롤러 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/constant/PlannerConstant.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/constant/PlannerConstant.java
@@ -6,4 +6,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PlannerConstant {
     public static final String SUCCESS_ADD_DAILY_TODO = "일일플래너 할일 등록이 완료되었습니다.";
+    public static final String SUCCESS_UPDATE_TODAY_GOAL = "오늘의 다짐 수정이 완료되었습니다.";
 }
--- BE/src/main/java/com/newsainturtle/shadowmate/planner/controller/PlannerController.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/controller/PlannerController.java
@@ -4,6 +4,7 @@ import com.newsainturtle.shadowmate.auth.service.AuthService;
 import com.newsainturtle.shadowmate.common.BaseResponse;
 import com.newsainturtle.shadowmate.config.auth.PrincipalDetails;
 import com.newsainturtle.shadowmate.planner.dto.AddDailyTodoRequest;
+import com.newsainturtle.shadowmate.planner.dto.UpdateTodayGoalRequest;
 import com.newsainturtle.shadowmate.planner.service.DailyPlannerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 import static com.newsainturtle.shadowmate.planner.constant.PlannerConstant.SUCCESS_ADD_DAILY_TODO;
+import static com.newsainturtle.shadowmate.planner.constant.PlannerConstant.SUCCESS_UPDATE_TODAY_GOAL;
 
 @RestController
 @RequestMapping("/api/planners")
@@ -28,6 +30,15 @@ public class PlannerController {
                                                      @RequestBody @Valid final AddDailyTodoRequest addDailyTodoRequest) {
         authServiceImpl.certifyUser(userId, principalDetails.getUser());
         return ResponseEntity.ok(BaseResponse.from(SUCCESS_ADD_DAILY_TODO, dailyPlannerServiceImpl.addDailyTodo(principalDetails.getUser(), addDailyTodoRequest)));
+    }
+
+    @PutMapping("/{userId}/daily/today-goals")
+    public ResponseEntity<BaseResponse> updateTodayGoal(@AuthenticationPrincipal final PrincipalDetails principalDetails,
+                                                        @PathVariable("userId") final Long userId,
+                                                        @RequestBody @Valid final UpdateTodayGoalRequest updateTodayGoalRequest) {
+        authServiceImpl.certifyUser(userId, principalDetails.getUser());
+        dailyPlannerServiceImpl.updateTodayGoal(principalDetails.getUser(), updateTodayGoalRequest);
+        return ResponseEntity.ok(BaseResponse.from(SUCCESS_UPDATE_TODAY_GOAL));
     }
 
 }
