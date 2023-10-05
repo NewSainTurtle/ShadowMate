628cb92394c4ad437c180d0fad8c8b1990c57605
yunaghgh <yunaghgh@naver.com>
2023-10-05 17:51:53 +0900

    [BE] feat: 일일플래너 할일 수정 서비스 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/exception/PlannerErrorResult.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/exception/PlannerErrorResult.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum PlannerErrorResult implements BaseErrorResult {
     INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "카테고리가 유효하지 않습니다."),
     INVALID_DAILY_PLANNER(HttpStatus.BAD_REQUEST, "플래너가 유효하지 않습니다."),
+    INVALID_TODO(HttpStatus.BAD_REQUEST, "플래너의 할일이 유효하지 않습니다."),
+    INVALID_TODO_STATUS(HttpStatus.BAD_REQUEST, "플래너의 할일 상태가 유효하지 않습니다."),
     ALREADY_ADDED_LIKE(HttpStatus.BAD_REQUEST, "이미 좋아요를 눌렀습니다."),
     UNABLE_TO_LIKE_YOUR_OWN_PLANNER(HttpStatus.BAD_REQUEST, "본인 플래너에 좋아요를 누를 수 없습니다."),
     ;
