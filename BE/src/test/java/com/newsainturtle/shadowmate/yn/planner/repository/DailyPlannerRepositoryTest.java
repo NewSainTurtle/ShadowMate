d4928f38b21ccb6c273d507fa1562345c52fe1bf
yunaghgh <yunaghgh@naver.com>
2023-10-05 15:58:52 +0900

    [BE] test: 오늘의회고 사진 업로드 레포지토리 테스트 코드 작성

--- BE/src/test/java/com/newsainturtle/shadowmate/yn/planner/repository/DailyPlannerRepositoryTest.java
+++ BE/src/test/java/com/newsainturtle/shadowmate/yn/planner/repository/DailyPlannerRepositoryTest.java
@@ -198,6 +198,73 @@ public class DailyPlannerRepositoryTest {
             assertThat(findDailyPlanner.getRetrospection()).isEqualTo("오늘 계획했던 일을 모두 끝냈다!!! 신남~~");
             assertThat(findDailyPlanner.getCreateTime()).isNotEqualTo(findDailyPlanner.getUpdateTime());
         }
+
+        @Test
+        public void 오늘의회고사진업로드_null() {
+            //given
+            dailyPlannerRepository.save(DailyPlanner.builder()
+                    .dailyPlannerDay(Date.valueOf("2023-09-25"))
+                    .user(user)
+                    .retrospectionImage("https://i.pinimg.com/564x/62/00/71/620071d0751e8cd562580a83ec834f7e.jpg")
+                    .build());
+            //when
+            final DailyPlanner dailyPlanner = dailyPlannerRepository.findByUserAndDailyPlannerDay(user, Date.valueOf("2023-09-25"));
+            final DailyPlanner changeDailyPlanner = DailyPlanner.builder()
+                    .id(dailyPlanner.getId())
+                    .createTime(dailyPlanner.getCreateTime())
+                    .dailyPlannerDay(dailyPlanner.getDailyPlannerDay())
+                    .user(dailyPlanner.getUser())
+                    .todayGoal(dailyPlanner.getTodayGoal())
+                    .tomorrowGoal(dailyPlanner.getTomorrowGoal())
+                    .retrospection(dailyPlanner.getRetrospection())
+                    .retrospectionImage(null)
+                    .build();
+            dailyPlannerRepository.save(changeDailyPlanner);
+            final DailyPlanner findDailyPlanner = dailyPlannerRepository.findByUserAndDailyPlannerDay(user, Date.valueOf("2023-09-25"));
+
+            //then
+            assertThat(findDailyPlanner).isNotNull();
+            assertThat(findDailyPlanner.getDailyPlannerDay()).isEqualTo(Date.valueOf("2023-09-25"));
+            assertThat(findDailyPlanner.getUser()).isEqualTo(user);
+            assertThat(findDailyPlanner.getTodayGoal()).isNull();
+            assertThat(findDailyPlanner.getTomorrowGoal()).isNull();
+            assertThat(findDailyPlanner.getRetrospectionImage()).isNull();
+            assertThat(findDailyPlanner.getRetrospection()).isNull();
+            assertThat(findDailyPlanner.getCreateTime()).isNotEqualTo(findDailyPlanner.getUpdateTime());
+        }
+
+        @Test
+        public void 오늘의회고사진업로드() {
+            //given
+            dailyPlannerRepository.save(DailyPlanner.builder()
+                    .dailyPlannerDay(Date.valueOf("2023-09-25"))
+                    .user(user)
+                    .build());
+            //when
+            final DailyPlanner dailyPlanner = dailyPlannerRepository.findByUserAndDailyPlannerDay(user, Date.valueOf("2023-09-25"));
+            final DailyPlanner changeDailyPlanner = DailyPlanner.builder()
+                    .id(dailyPlanner.getId())
+                    .createTime(dailyPlanner.getCreateTime())
+                    .dailyPlannerDay(dailyPlanner.getDailyPlannerDay())
+                    .user(dailyPlanner.getUser())
+                    .todayGoal(dailyPlanner.getTodayGoal())
+                    .tomorrowGoal(dailyPlanner.getTomorrowGoal())
+                    .retrospection(dailyPlanner.getRetrospection())
+                    .retrospectionImage("https://i.pinimg.com/564x/62/00/71/620071d0751e8cd562580a83ec834f7e.jpg")
+                    .build();
+            dailyPlannerRepository.save(changeDailyPlanner);
+            final DailyPlanner findDailyPlanner = dailyPlannerRepository.findByUserAndDailyPlannerDay(user, Date.valueOf("2023-09-25"));
+
+            //then
+            assertThat(findDailyPlanner).isNotNull();
+            assertThat(findDailyPlanner.getDailyPlannerDay()).isEqualTo(Date.valueOf("2023-09-25"));
+            assertThat(findDailyPlanner.getUser()).isEqualTo(user);
+            assertThat(findDailyPlanner.getTodayGoal()).isNull();
+            assertThat(findDailyPlanner.getTomorrowGoal()).isNull();
+            assertThat(findDailyPlanner.getRetrospectionImage()).isEqualTo("https://i.pinimg.com/564x/62/00/71/620071d0751e8cd562580a83ec834f7e.jpg");
+            assertThat(findDailyPlanner.getRetrospection()).isNull();
+            assertThat(findDailyPlanner.getCreateTime()).isNotEqualTo(findDailyPlanner.getUpdateTime());
+        }
     }
 
     @Nested
