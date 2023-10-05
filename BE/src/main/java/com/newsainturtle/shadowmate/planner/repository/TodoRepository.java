7b15f5590b42fa8078049dd06cee0540a5f141df
yunaghgh <yunaghgh@naver.com>
2023-10-06 00:27:31 +0900

    [BE] feat: 해당 카테고리로 등록된 할일 개수 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/TodoRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/TodoRepository.java
@@ -2,10 +2,12 @@ package com.newsainturtle.shadowmate.planner.repository;
 
 import com.newsainturtle.shadowmate.planner.entity.DailyPlanner;
 import com.newsainturtle.shadowmate.planner.entity.Todo;
+import com.newsainturtle.shadowmate.planner_setting.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     void deleteByIdAndAndDailyPlanner(final Long id, final DailyPlanner dailyPlanner);
+    long countByCategory(final Category category);
 }
