b02339549b04a2e06b96ec47658b4ab4267a81cd
yunaghgh <yunaghgh@naver.com>
2023-10-06 21:05:03 +0900

    [BE] test: 주차별 플래너 등록 레포지토리 테스트 코드 작성

---
+++ BE/src/test/java/com/newsainturtle/shadowmate/yn/planner/repository/WeeklyRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.newsainturtle.shadowmate.yn.planner.repository;
+
+import com.newsainturtle.shadowmate.planner.entity.Weekly;
+import com.newsainturtle.shadowmate.planner.repository.WeeklyRepository;
+import com.newsainturtle.shadowmate.user.entity.User;
+import com.newsainturtle.shadowmate.user.enums.PlannerAccessScope;
+import com.newsainturtle.shadowmate.user.enums.SocialType;
+import com.newsainturtle.shadowmate.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.sql.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class WeeklyRepositoryTest {
+
+    @Autowired
+    private WeeklyRepository weeklyRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private final Date startDay = Date.valueOf("2023-10-09");
+    private final Date endDay = Date.valueOf("2023-10-15");
+    private User user;
+
+    @BeforeEach
+    public void init() {
+        user = userRepository.save(User.builder()
+                .email("test1234@naver.com")
+                .password("123456")
+                .socialLogin(SocialType.BASIC)
+                .nickname("거북이")
+                .plannerAccessScope(PlannerAccessScope.PUBLIC)
+                .withdrawal(false)
+                .build());
+    }
+
+    @Test
+    public void 주차별플래너등록() {
+        //given
+
+        //when
+        final Weekly weekly = weeklyRepository.save(Weekly.builder()
+                .startDay(startDay)
+                .endDay(endDay)
+                .user(user)
+                .build());
+
+        //then
+        assertThat(weekly).isNotNull();
+        assertThat(weekly.getUser()).isEqualTo(user);
+        assertThat(weekly.getStartDay()).isEqualTo(startDay);
+        assertThat(weekly.getEndDay()).isEqualTo(endDay);
+    }
+
+}
