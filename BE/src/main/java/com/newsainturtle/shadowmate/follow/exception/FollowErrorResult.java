33f1b443a925d7ecdda20e3ccada11f1f3ab15e8
tgb02087 <tgb02087@naver.com>
2023-10-07 04:19:32 +0900

    [BE] refactor : ErrorResult 삭제

--- BE/src/main/java/com/newsainturtle/shadowmate/follow/exception/FollowErrorResult.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/follow/exception/FollowErrorResult.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum FollowErrorResult implements BaseErrorResult {
 
-    NOTFOUND_FOLLOWING(HttpStatus.NOT_FOUND, "등록된 팔로잉이 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
