4ffacfae65391cbcad5f1e8c1dcd0d8b70c1522b
tgb02087 <tgb02087@naver.com>
2023-10-08 03:07:46 +0900

    [BE] feat : Follow친구신청 repository

---
+++ BE/src/main/java/com/newsainturtle/shadowmate/follow/repository/FollowRequestRepository.java
@@ -0,0 +1,8 @@
+package com.newsainturtle.shadowmate.follow.repository;
+
+import com.newsainturtle.shadowmate.follow.entity.FollowRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRequestRepository extends JpaRepository<FollowRequest, Long> {
+
+}
