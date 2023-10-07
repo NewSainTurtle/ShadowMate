657729186737e08b72fda9521e6c2b2d34fa112e
tgb02087 <tgb02087@naver.com>
2023-10-08 03:06:07 +0900

    [BE] test : Follow친구 신청 repository test코드 추가

---
+++ BE/src/test/java/com/newsainturtle/shadowmate/kh/follow/repository/FollowRequestRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.newsainturtle.shadowmate.kh.follow.repository;
+
+import com.newsainturtle.shadowmate.follow.entity.FollowRequest;
+import com.newsainturtle.shadowmate.follow.repository.FollowRequestRepository;
+import com.newsainturtle.shadowmate.user.entity.User;
+import com.newsainturtle.shadowmate.user.enums.PlannerAccessScope;
+import com.newsainturtle.shadowmate.user.enums.SocialType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class FollowRequestRepositoryTest {
+
+    @Autowired
+    private FollowRequestRepository followRequestRepository;
+
+    final User user1 = User.builder()
+            .email("test1@test.com")
+            .password("123456")
+            .socialLogin(SocialType.BASIC)
+            .nickname("거북이1")
+            .plannerAccessScope(PlannerAccessScope.PUBLIC)
+            .withdrawal(false)
+            .build();
+
+    final User user2 = User.builder()
+            .email("test2@test.com")
+            .password("123456")
+            .socialLogin(SocialType.BASIC)
+            .nickname("거북이2")
+            .plannerAccessScope(PlannerAccessScope.PUBLIC)
+            .withdrawal(false)
+            .build();
+
+
+    @Test
+    public void 성공_팔로우신청_비공개() {
+        //given
+        FollowRequest followRequest = FollowRequest.builder()
+                .requesterId(user1)
+                .receiverId(user2)
+                .build();
+
+        //when
+        FollowRequest result = followRequestRepository.save(followRequest);
+
+        //then
+        assertThat(result.getRequesterId()).isEqualTo(user1);
+        assertThat(result.getReceiverId()).isEqualTo(user2);
+    }
+}
