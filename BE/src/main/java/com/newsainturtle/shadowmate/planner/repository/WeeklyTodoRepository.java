fe8709cc2aa6367d18e7c7e90dbe4711d3c5d096
yunaghgh <yunaghgh@naver.com>
2023-10-07 15:26:47 +0900

    [BE] feat: 주차별 할일 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/WeeklyTodoRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/WeeklyTodoRepository.java
@@ -1,7 +1,9 @@
 package com.newsainturtle.shadowmate.planner.repository;
 
+import com.newsainturtle.shadowmate.planner.entity.Weekly;
 import com.newsainturtle.shadowmate.planner.entity.WeeklyTodo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WeeklyTodoRepository extends JpaRepository<WeeklyTodo, Long> {
+    WeeklyTodo findByIdAndWeekly(final Long id, final Weekly weekly);
 }
