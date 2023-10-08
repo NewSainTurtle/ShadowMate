776e6a6c1bba07b3cbb7e0a7e713c6f2465e7fb2
taboowiths <tabou9820@gmail.com>
2023-10-09 00:58:27 +0900

    [FE] feat: 마이페이지 디데이 목록 날짜 출력 형식 함수 정의

--- FE/src/components/mypage/item/MyPageDdayItem.tsx
+++ FE/src/components/mypage/item/MyPageDdayItem.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch, SetStateAction } from "react";
 import styles from "@styles/mypage/MyPage.module.scss";
 import Text from "@components/common/Text";
 import { ddayType } from "@util/planner.interface";
+import Dday from "@components/common/Dday";
 
 interface Props {
   item: ddayType;
@@ -10,17 +11,30 @@ interface Props {
   setClick: Dispatch<SetStateAction<number>>;
 }
 
+const dateFormat = (dateInfo: Date | string) => {
+  const dayList = ["일", "월", "화", "수", "목", "금", "토"];
+
+  let today = new Date(dateInfo);
+  let year = today.getFullYear();
+  let month = String(today.getMonth() + 1);
+  let date = String(today.getDate());
+  let day = dayList[today.getDay()];
+
+  month = month.length > 1 ? month : "0" + month;
+  date = date.length > 1 ? date : "0" + date;
+
+  return year + "." + month + "." + date + "(" + day + ")";
+};
+
 const MyPageDdayItem = ({ item, index, click, setClick }: Props) => {
   const clicked = click === index ? "--clicked" : "";
   return (
-    <div className={styles[`dday-item${clicked}`]} onClick={() => setClick(index)}>
+    <div className={styles[`dday__item${clicked}`]} onClick={() => setClick(index)}>
       <div className={styles["dday__item__title"]}>
         <Text>{item.ddayTitle}</Text>
-        <Text types="small">{item.ddayDate.toString()}</Text>
+        <Text types="small">{dateFormat(item.ddayDate)}</Text>
       </div>
-      <Text types="semi-medium" bold>
-        D-234
-      </Text>
+      <Dday comparedDate={item.ddayDate}></Dday>
     </div>
   );
 };
