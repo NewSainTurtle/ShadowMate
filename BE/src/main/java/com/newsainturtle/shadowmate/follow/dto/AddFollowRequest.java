f5504e15c325599c7fdd2343732e37bf9c04b8ad
tgb02087 <tgb02087@naver.com>
2023-10-09 15:59:06 +0900

    [BE] refactor : 생성자 수정

--- BE/src/main/java/com/newsainturtle/shadowmate/follow/dto/AddFollowRequest.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/follow/dto/AddFollowRequest.java
@@ -1,14 +1,21 @@
 package com.newsainturtle.shadowmate.follow.dto;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AddFollowRequest {
 
     @NotNull
     private Long followingId;
+
+    @Builder
+    public AddFollowRequest(Long followingId) {
+        this.followingId = followingId;
+    }
 }
