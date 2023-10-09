adf79380319b993c47c483f408b94ed4b9be6a9f
tgb02087 <tgb02087@naver.com>
2023-10-09 14:32:49 +0900

    [BE] refactor : NotNull조건 추가

--- BE/src/main/java/com/newsainturtle/shadowmate/follow/dto/AddFollowRequest.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/follow/dto/AddFollowRequest.java
@@ -3,9 +3,12 @@ package com.newsainturtle.shadowmate.follow.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Builder
 public class AddFollowRequest {
 
+    @NotNull
     private Long followingId;
 }
