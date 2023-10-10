7bf81bfccb105b9454c45619a487974c6c798236
dustnzlzl <dustnzlzl@naver.com>
2023-10-10 13:51:57 +0900

    [FE] feat: 친구 목록 탭 보이기

--- FE/src/components/mypage/details/MyPageFriend.tsx
+++ FE/src/components/mypage/details/MyPageFriend.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "@styles/mypage/MyPage.module.scss";
 
 const MyPageFriend = () => {
-  return <div>친구목록</div>;
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const menuArr = [
+    { name: "팔로워", node: "팔로워 목록" },
+    { name: "팔로잉", node: "팔로잉 목록" },
+  ];
+
+  return (
+    <div className={styles["friend__cantainer"]}>
+      <div className={styles["friend__tab"]}>
+        {menuArr.map((item, idx) => (
+          <li
+            className={`${styles["friend__subtab"]} ${idx === currentTab ? styles["friend__subtab--focused"] : ""}`}
+            key={idx}
+            onClick={() => setCurrentTab(idx)}
+          >
+            {item.name}
+          </li>
+        ))}
+      </div>
+      <div>
+        <p>{menuArr[currentTab].node}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MyPageFriend;
