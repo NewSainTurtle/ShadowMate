46bbb40471e436b9fe8b9bc7e74340a785ec44e6
yunaghgh <yunaghgh@naver.com>
2023-10-10 15:19:49 +0900

    [BE] feat: 일일플래너 조회 서비스 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/service/DailyPlannerServiceImpl.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/service/DailyPlannerServiceImpl.java
@@ -13,7 +13,9 @@ import com.newsainturtle.shadowmate.planner.repository.DailyPlannerRepository;
 import com.newsainturtle.shadowmate.planner.repository.TimeTableRepository;
 import com.newsainturtle.shadowmate.planner.repository.TodoRepository;
 import com.newsainturtle.shadowmate.planner_setting.entity.Category;
+import com.newsainturtle.shadowmate.planner_setting.entity.Dday;
 import com.newsainturtle.shadowmate.planner_setting.repository.CategoryRepository;
+import com.newsainturtle.shadowmate.planner_setting.repository.DdayRepository;
 import com.newsainturtle.shadowmate.user.entity.User;
 import com.newsainturtle.shadowmate.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,8 +23,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -35,6 +42,7 @@ public class DailyPlannerServiceImpl implements DailyPlannerService {
     private final DailyPlannerLikeRepository dailyPlannerLikeRepository;
     private final TimeTableRepository timeTableRepository;
     private final UserRepository userRepository;
+    private final DdayRepository ddayRepository;
 
     private DailyPlanner getOrCreateDailyPlanner(final User user, final String date) {
         DailyPlanner dailyPlanner = dailyPlannerRepository.findByUserAndDailyPlannerDay(user, Date.valueOf(date));
@@ -190,7 +198,7 @@ public class DailyPlannerServiceImpl implements DailyPlannerService {
         }
 
         final User plannerWriter = userRepository.findByIdAndWithdrawalIsFalse(plannerWriterId);
-        if(plannerWriter == null){
+        if (plannerWriter == null) {
             throw new PlannerException(PlannerErrorResult.INVALID_USER);
         }
 
@@ -268,5 +276,82 @@ public class DailyPlannerServiceImpl implements DailyPlannerService {
         final long timeTableId = todo.getTimeTable().getId();
         todo.setTimeTable(null);
         timeTableRepository.deleteById(timeTableId);
+    }
+
+    private String LocalDateTimeToString(final LocalDateTime time) {
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return time.format(formatter);
+    }
+
+    private String getDday(final User user) {
+        final Date today = Date.valueOf(LocalDate.now());
+        Dday dday = ddayRepository.findTopByUserAndDdayDateGreaterThanEqualOrderByDdayDateAsc(user, today);
+        if (dday == null) dday = ddayRepository.findTopByUserAndDdayDateBeforeOrderByDdayDateDesc(user, today);
+        return dday == null ? null : dday.getDdayDate().toString();
+    }
+
+    @Override
+    public SearchDailyPlannerResponse searchDailyPlanner(final User user, final Long plannerWriterId, final String date) {
+        final String datePattern = "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$";
+        if (!Pattern.matches(datePattern, date)) {
+            throw new PlannerException(PlannerErrorResult.INVALID_DATE_FORMAT);
+        }
+
+        final User plannerWriter = userRepository.findByIdAndWithdrawalIsFalse(plannerWriterId);
+        if (plannerWriter == null) {
+            throw new PlannerException(PlannerErrorResult.INVALID_USER);
+        }
+
+        final DailyPlanner dailyPlanner = dailyPlannerRepository.findByUserAndDailyPlannerDay(plannerWriter, Date.valueOf(date));
+        int totalMinutes = 0;
+
+        if (dailyPlanner == null) {
+            return SearchDailyPlannerResponse.builder()
+                    .date(date)
+                    .dday(getDday(user))
+                    .build();
+        } else {
+            final boolean like = dailyPlannerLikeRepository.findByUserAndDailyPlanner(user, dailyPlanner) != null;
+            final long likeCount = dailyPlannerLikeRepository.countByDailyPlanner(dailyPlanner);
+            final List<Todo> todoList = todoRepository.findAllByDailyPlanner(dailyPlanner);
+
+            final List<DailyPlannerTodo> dailyTodo = new ArrayList<>();
+            for (Todo todo : todoList) {
+                dailyTodo.add(DailyPlannerTodo.builder()
+                        .todoId(todo.getId())
+                        .category(todo.getCategory() != null ? DailyPlannerTodoCategory.builder()
+                                .categoryId(todo.getCategory().getId())
+                                .categoryTitle(todo.getCategory().getCategoryTitle())
+                                .categoryColorCode(todo.getCategory().getCategoryColor().getCategoryColorCode())
+                                .categoryEmoticon(todo.getCategory().getCategoryEmoticon())
+                                .build() : null)
+                        .todoContent(todo.getTodoContent())
+                        .todoStatus(todo.getTodoStatus().getStatus())
+                        .timeTable(todo.getTimeTable() != null ? DailyPlannerTodoTimeTable.builder()
+                                .timeTableId(todo.getTimeTable().getId())
+                                .startTime(LocalDateTimeToString(todo.getTimeTable().getStartTime()))
+                                .endTime(LocalDateTimeToString(todo.getTimeTable().getEndTime()))
+                                .build() : null)
+                        .build());
+                if (todo.getTimeTable() != null) {
+                    totalMinutes += ChronoUnit.MINUTES.between(todo.getTimeTable().getStartTime(), todo.getTimeTable().getEndTime());
+                }
+            }
+
+            return SearchDailyPlannerResponse.builder()
+                    .date(date)
+                    .dday(getDday(user))
+                    .todayGoal(dailyPlanner.getTodayGoal())
+                    .retrospection(dailyPlanner.getRetrospection())
+                    .retrospectionImage(dailyPlanner.getRetrospectionImage())
+                    .tomorrowGoal(dailyPlanner.getTomorrowGoal())
+                    .like(like)
+                    .likeCount(likeCount)
+                    .studyTimeHour(totalMinutes / 60)
+                    .studyTimeMinute(totalMinutes % 60)
+                    .dailyTodo(dailyTodo)
+                    .build();
+
+        }
     }
 }
