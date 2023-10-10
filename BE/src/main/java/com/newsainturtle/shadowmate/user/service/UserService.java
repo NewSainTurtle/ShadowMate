24919fb245b5512701226e2d3495e673780cc0e7
tgb02087 <tgb02087@naver.com>
2023-10-10 15:16:03 +0900

    [BE] feat: 회원검색 Service API 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/user/service/UserService.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/user/service/UserService.java
@@ -1,11 +1,14 @@
 package com.newsainturtle.shadowmate.user.service;
 
 import com.newsainturtle.shadowmate.user.dto.ProfileResponse;
+import com.newsainturtle.shadowmate.user.entity.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
 
     ProfileResponse getProfile(final Long userId);
+
+    User searchNickname(final String nickname);
 
 }
