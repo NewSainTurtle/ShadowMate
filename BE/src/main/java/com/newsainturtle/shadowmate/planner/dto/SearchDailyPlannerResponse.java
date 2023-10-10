1b325432f3a36b9f61a95a977095d564e81d6648
yunaghgh <yunaghgh@naver.com>
2023-10-10 15:17:43 +0900

    [BE] feat: 일일플래너 조회 dto 정의

---
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/dto/SearchDailyPlannerResponse.java
@@ -0,0 +1,24 @@
+package com.newsainturtle.shadowmate.planner.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class SearchDailyPlannerResponse {
+
+    private String date;
+    private String dday;
+    private String todayGoal;
+    private String retrospection;
+    private String retrospectionImage;
+    private String tomorrowGoal;
+    private boolean like;
+    private long likeCount;
+    private int studyTimeHour;
+    private int studyTimeMinute;
+    private List<DailyPlannerTodo> dailyTodo;
+
+}
