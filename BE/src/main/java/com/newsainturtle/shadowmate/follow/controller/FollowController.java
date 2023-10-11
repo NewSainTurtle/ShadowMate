9a9e4f562698bd526fd6ae148c50fd05357a1163
tgb02087 <tgb02087@naver.com>
2023-10-12 05:49:21 +0900

    [BE] feat : 친구신청응답 Controller API 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/follow/controller/FollowController.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/follow/controller/FollowController.java
@@ -6,6 +6,7 @@ import com.newsainturtle.shadowmate.config.auth.PrincipalDetails;
 import com.newsainturtle.shadowmate.follow.dto.AddFollowRequest;
 import com.newsainturtle.shadowmate.follow.dto.DeleteFollowRequestRequest;
 import com.newsainturtle.shadowmate.follow.dto.DeleteFollowerRequest;
+import com.newsainturtle.shadowmate.follow.dto.ReceiveFollowRequest;
 import com.newsainturtle.shadowmate.follow.service.FollowServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -63,6 +64,14 @@ public class FollowController {
         authService.certifyUser(userId, principalDetails.getUser());
         followService.deleteFollowRequest(principalDetails.getUser(), deleteFollowRequestRequest.getReceiverId());
         return ResponseEntity.ok(BaseResponse.from(SUCCESS_DELETE_FOLLOW_REQUEST));
+    }
+
+    @PostMapping("/{userId}/receive")
+    public ResponseEntity<BaseResponse> receiveFollow(@AuthenticationPrincipal final PrincipalDetails principalDetails,
+                                                            @PathVariable("userId") final Long userId,
+                                                            @RequestBody @Valid final ReceiveFollowRequest receiveFollowRequest) {
+        authService.certifyUser(userId, principalDetails.getUser());
+        return ResponseEntity.ok(BaseResponse.from(followService.receiveFollow(principalDetails.getUser(), receiveFollowRequest.getRequesterId(), receiveFollowRequest.isFollowReceive())));
     }
 
 }
