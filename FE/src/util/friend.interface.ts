03dafb000150415ccca517b2c4a12d13644cac51
dustnzlzl <dustnzlzl@naver.com>
2023-10-11 15:09:17 +0900

    [FE] feat: 친구 interface 세팅

--- FE/src/util/friend.interface.ts
+++ FE/src/util/friend.interface.ts
@@ -6,23 +6,30 @@ interface friendInfo {
   statusMessage: string;
 }
 
-// 팔로잉 목록(내가 받은 모든)
-export interface following extends friendInfo {
+interface isfollow {
+  isfollow: "취소" | "삭제" | "팔로워신청";
+}
+
+// 팔로워 목록
+export interface followerType extends friendInfo, isfollow {
   followId: number;
   followerId: number;
 }
 
-// 팔로워 목록(내가 신청한 모든)
-export interface follower extends following {
-  isfollow: boolean;
+// 팔로잉 목록
+export interface followingType extends friendInfo {
+  followId: number;
+  followerId: number;
 }
 
-// 팔로우 요청 목록(내가 신청한 사람만)
-export interface followSendType extends friendInfo {
+// 팔로우 요청 목록
+export interface followRequestType extends friendInfo {
   followRequestId: number;
+  receiveId: number;
 }
 
-// 팔로우 받은 목록(나한테 친구 신청했는데 승락 안 한 사람..?)
-export interface followGetType extends followSendType {
-  receiveId: number;
+// 친구 검색
+
+export interface friendSearchType extends friendInfo, isfollow {
+  userId: number;
 }
