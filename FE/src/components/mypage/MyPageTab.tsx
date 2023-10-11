8a82e644065d67aae608ca7bddb178a16520f282
taboowiths <tabou9820@gmail.com>
2023-10-11 19:25:22 +0900

    [FE] feat: 마이페이지 탭 하위 카테고리 클릭 시 상위 카테고리 색상 변경

--- FE/src/components/mypage/MyPageTab.tsx
+++ FE/src/components/mypage/MyPageTab.tsx
@@ -6,36 +6,55 @@ interface MyPageTabProps {
   setTabName: Dispatch<SetStateAction<string>>;
 }
 
-const MENU_LIST = [
-  { id: "info", title: "내 정보", list: ["내 정보 확인"] },
-  { id: "diary", title: "다이어리", list: ["다이어리 설정", "카테고리 설정", "디데이 설정"] },
-  { id: "friend", title: "친구", list: ["팔로워 목록", "팔로잉 목록", "친구 검색"] },
+interface TabListType {
+  id: string;
+  title: string;
+  contents: {
+    index: number;
+    list: string[];
+  };
+}
+
+const TAB_LIST: TabListType[] = [
+  { id: "info", title: "내 정보", contents: { index: 0, list: ["내 정보 확인"] } },
+  {
+    id: "diary",
+    title: "다이어리",
+    contents: { index: 1, list: ["다이어리 설정", "카테고리 설정", "디데이 설정"] },
+  },
+  { id: "friend", title: "친구", contents: { index: 2, list: ["팔로워 목록", "팔로잉 목록", "친구 검색"] } },
 ];
 
 const MyPageTab = ({ setTabName }: MyPageTabProps) => {
+  const [headerIndex, setHeaderIndex] = useState<number>(0); // 상위 카테고리
   const [subIndex, setSubIndex] = useState<string>("내 정보 확인"); // 하위 카테고리
 
-  const handleLink = (list: string) => {
+  const handleLink = (list: string, idx: number) => {
+    setHeaderIndex(idx);
     setSubIndex(list);
     setTabName(list);
   };
 
   return (
     <div className={styles["tab"]}>
-      {MENU_LIST.map((item, idx) => {
+      {TAB_LIST.map((item, idx) => {
+        const clicked = idx === headerIndex ? "--clicked" : "";
         return (
-          <div key={idx} className={styles[`tab__title`]}>
+          <div key={idx} className={styles[`tab__title${clicked}`]}>
             <input type="checkbox" id={item.id} checked />
             <label htmlFor={item.id}>{item.title}</label>
-            {item.list && (
+            {item.contents.list && (
               <div className={styles["tab__contents"]}>
-                {item.list?.map((list, idx) => (
-                  <div onClick={() => handleLink(list)} key={idx}>
-                    <Text types="small" bold={list === subIndex}>
-                      {list}
-                    </Text>
-                  </div>
-                ))}
+                {item.contents.list.map((list, idx) => {
+                  const cur = item.contents.index;
+                  return (
+                    <div onClick={() => handleLink(list, cur)} key={idx}>
+                      <Text types="small" bold={list === subIndex}>
+                        {list}
+                      </Text>
+                    </div>
+                  );
+                })}
               </div>
             )}
           </div>
