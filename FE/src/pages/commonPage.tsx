d3bb21491224e84cc5705999b9afd524c11937a0
dustnzlzl <dustnzlzl@naver.com>
2023-10-11 14:14:36 +0900

    [FE] feat: 공통 친구프로필 컴포넌트 타입 추가

--- FE/src/pages/commonPage.tsx
+++ FE/src/pages/commonPage.tsx
@@ -46,10 +46,12 @@ const commonPage = () => {
       <br />
       <div>
         <FriendProfile types="기본" profile={profileInfo} />
-        <FriendProfile types="삭제" profile={profileInfo} />
-        <FriendProfile types="추가" profile={profileInfo} />
-        <FriendProfile types="요청" profile={profileInfo} />
         <FriendProfile types="아이콘" profile={profileInfo} />
+        <FriendProfile types="삭제" profile={profileInfo} />
+        <FriendProfile types="친구 신청" profile={profileInfo} />
+        <FriendProfile types="팔로워 신청" profile={profileInfo} />
+        <FriendProfile types="요청" profile={profileInfo} />
+        <FriendProfile types="취소" profile={profileInfo} />
       </div>
       <br />
       <div>
