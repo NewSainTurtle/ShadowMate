5c0d64a0a756323d77793bcc931c86629bb9980c
tgb02087 <tgb02087@naver.com>
2023-10-11 15:39:10 +0900

    [BE] feat: 친구신청취소 constant API 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/follow/constant/FollowConstant.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/follow/constant/FollowConstant.java
@@ -9,6 +9,6 @@ public class FollowConstant {
     public static final String SUCCESS_ADD_FOLLOW = "팔로우 신청이 완료되었습니다.";
     public static final String SUCCESS_GET_FOLLOWING_LIST = "팔로잉 목록 조회가 완료되었습니다.";
     public static final String SUCCESS_GET_FOLLOWER_LIST = "팔로워 목록 조회가 완료되었습니다.";
-
     public static final String SUCCESS_DELETE_FOLLOWER = "팔로워 삭제가 완료되었습니다.";
+    public static final String SUCCESS_DELETE_FOLLOW_REQUEST = "친구신청 취소가 완료되었습니다.";
 }
