4fd86c749ae1b2011b67a973025ecc04d2b08e7d
taboowiths <tabou9820@gmail.com>
2023-10-11 14:18:39 +0900

    [FE] refactor: 마이페이지 다이어리 공개 설정 라디오버튼으로 수정

--- FE/src/components/mypage/details/diary/Diary.tsx
+++ FE/src/components/mypage/details/diary/Diary.tsx
@@ -1,32 +1,30 @@
-import React, { useState } from "react";
+import React, { Children, useState } from "react";
 import styles from "@styles/mypage/MyPage.module.scss";
 import Text from "@components/common/Text";
-import { Stack } from "@mui/material";
-import { SwitchButton } from "@components/common/Header/Toggle";
-import LockIcon from "@mui/icons-material/Lock";
-import LockOpenIcon from "@mui/icons-material/LockOpen";
+import { FormControlLabel, Radio, RadioGroup, Stack } from "@mui/material";
+import RadioButton from "@components/common/RadioButton";
 
 const MyPageDiary = () => {
-  const [open, setOpen] = useState(false);
-  const onChange = () => {
-    setOpen(!open);
-  };
-
   return (
     <div className={styles["diary__contents"]}>
       <div className={styles["diary__line"]}>
         <Text>다이어리 공개</Text>
-        <span>
-          <Text types="small">다이어리 공개 시 소셜에 작성된 플래너가 공유됩니다.</Text>
-          <div className={styles["toggle"]}>
+        <div>
+          <Text types="small">다이어리 공개 시 작성된 플래너가 소셜에 공유됩니다.</Text>
+          <div className={styles["radio"]}>
             <Stack direction="row" spacing={1} alignItems="center">
-              <span className={styles["toggle__icon"]}>
-                {open ? <LockOpenIcon sx={{ fontSize: "medium" }} /> : <LockIcon sx={{ fontSize: "medium" }} />}
-              </span>
-              <SwitchButton color="info" checked={open} onChange={onChange} />
+              <RadioGroup defaultValue="close" name="radio-buttons-group">
+                <FormControlLabel value="close" control={<RadioButton />} label={<Text types="small">비공개</Text>} />
+                <FormControlLabel
+                  value="friendOpen"
+                  control={<RadioButton />}
+                  label={<Text types="small">친구 공개</Text>}
+                />
+                <FormControlLabel value="open" control={<RadioButton />} label={<Text types="small">전체 공개</Text>} />
+              </RadioGroup>
             </Stack>
           </div>
-        </span>
+        </div>
       </div>
     </div>
   );
