52ebf3e4d286fca2d4378942409052b7661046aa
tgb02087 <tgb02087@naver.com>
2023-10-13 00:46:18 +0900

    [BE] feat : 친구신청목록 조회 dto API 작성

---
+++ BE/src/main/java/com/newsainturtle/shadowmate/follow/dto/FollowRequestResponse.java
@@ -0,0 +1,24 @@
+package com.newsainturtle.shadowmate.follow.dto;
+
+import com.newsainturtle.shadowmate.user.enums.PlannerAccessScope;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class FollowRequestResponse {
+
+    private Long followRequestId;
+
+    private Long requesterId;
+
+    private String email;
+
+    private String nickname;
+
+    private String profileImage;
+
+    private String statusMessage;
+
+    private PlannerAccessScope plannerAccessScope;
+}
