c8db062ea46bfdb2c87b74b1c2982a290f753146
dustnzlzl <dustnzlzl@naver.com>
2023-10-17 04:49:02 +0900

    [FE] feat: 플래너 할 일 드래그 구현하기

--- FE/src/components/planner/day/TimeTable.tsx
+++ FE/src/components/planner/day/TimeTable.tsx
@@ -1,6 +1,9 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
 import styles from "@styles/planner/day.module.scss";
 import dayjs from "dayjs";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "@hooks/hook";
+import { removeTodo, selectTodo } from "@store/planner/daySlice";
 
 interface Props {
   date: string | Date | dayjs.Dayjs;
@@ -8,41 +11,105 @@ interface Props {
   setClicked: (props: boolean) => void;
 }
 
+interface tableTimeType {
+  todoId: number;
+  categoryColorCode: string;
+  time: string;
+}
+
 const TimeTable = ({ date, clicked, setClicked }: Props) => {
+  const dispatch = useDispatch();
+  const { todoId, categoryColorCode } = useAppSelector(selectTodo);
   const plannerDate = dayjs(date).startOf("d").format("YYYY-MM-DD");
   const plannerTime = {
+    // 오전 4시 ~ 익일 4시
     start: dayjs(plannerDate).set("h", 4).format("YYYY-MM-DD HH:mm"),
     end: dayjs(plannerDate).add(1, "day").set("h", 4).format("YYYY-MM-DD HH:mm"),
   };
-  const timeArr = useMemo(() => {
-    const time = [];
+  const makeTimeArr: tableTimeType[] = (() => {
+    const time: tableTimeType[] = [];
     let tempTime = plannerTime.start;
-    let id = 0;
     while (tempTime != plannerTime.end) {
       tempTime = dayjs(tempTime).add(10, "m").format("YYYY-MM-DD HH:mm");
-      time.push({ id, time: dayjs(tempTime).format("YYYY-MM-DD HH:mm") });
-      id++;
+      time.push({ todoId: 0, categoryColorCode: "", time: tempTime });
     }
     return time;
-  }, []);
+  })();
+
+  const [timeArr, setTimeArr] = useState<tableTimeType[]>(makeTimeArr);
+  const [timeClick, setTimeClicked] = useState(false);
+  const [selectTime, setSelectTime] = useState({
+    startTime: "",
+    endTime: "",
+  });
+
+  useEffect(() => {
+    if (selectTime.startTime != "" && selectTime.endTime != "") {
+      let { startTime, endTime } = selectTime;
+      if (startTime > endTime) {
+        const temp = startTime;
+        startTime = endTime;
+        endTime = temp;
+      }
+
+      const dragTime: tableTimeType[] = timeArr.reduce((updateArr: tableTimeType[], obj) => {
+        if (obj.time >= startTime && obj.time <= endTime) {
+          updateArr.push({ ...obj, todoId, categoryColorCode });
+        }
+        return updateArr;
+      }, []);
+
+      const startIndex = timeArr.findIndex((e) => e.time == startTime);
+      const copyTimeArr = [...timeArr];
+      copyTimeArr.splice(startIndex, dragTime.length, ...dragTime);
+      setTimeArr(copyTimeArr);
+    }
+  }, [selectTime]);
+
+  const mouseModule = (() => {
+    const mouseDown = (e: React.MouseEvent<HTMLDivElement>, time: string) => {
+      setTimeClicked(true);
+      setSelectTime({ ...selectTime, startTime: time });
+    };
+    const mouseEnter = (e: React.MouseEvent<HTMLDivElement>, time: string) => {
+      if (timeClick) {
+        setSelectTime({ ...selectTime, endTime: time });
+      }
+    };
+
+    const mouseUp = (e: React.MouseEvent<HTMLDivElement>, time: string) => {
+      setTimeClicked(false);
+      setSelectTime({ startTime: "", endTime: "" });
+    };
+
+    return {
+      mouseDown,
+      mouseEnter,
+      mouseUp,
+    };
+  })();
 
   return (
     <div className={styles["timetable__container"]}>
       <div className={styles["timetable__container-box"]}>
         <div className={styles["timetable__hours"]}>
-          {Array.from({ length: 24 }).map((_, index) => (
-            <div key={index}>{String((4 + index) % 24).padStart(2, "0")}</div>
+          {Array.from({ length: 24 }).map((_, idx) => (
+            <div key={idx}>{String((4 + idx) % 24).padStart(2, "0")}</div>
           ))}
         </div>
         <div className={styles["timetable__minutes"]}>
-          {timeArr.map((item) => (
+          {timeArr.map((item, idx) => (
             <div
-              key={item.id}
+              key={idx}
               className={styles["timetable__minutes__item"]}
-              draggable
-              onClick={() => {
-                console.log(item.time);
+              onMouseDown={(e) => {
+                mouseModule.mouseDown(e, item.time);
               }}
+              onMouseEnter={(e) => mouseModule.mouseEnter(e, item.time)}
+              onMouseUp={(e) => {
+                mouseModule.mouseUp(e, item.time);
+              }}
+              style={{ backgroundColor: item.categoryColorCode }}
             />
           ))}
         </div>
