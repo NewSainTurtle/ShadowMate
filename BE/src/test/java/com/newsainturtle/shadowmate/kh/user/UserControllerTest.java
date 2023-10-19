806eba819ba19fdf0565be93df02582769d5d5ca
tgb02087 <tgb02087@naver.com>
2023-10-20 03:23:00 +0900

    [BE] test : 프로필이미지수정 controller Test 작성

--- BE/src/test/java/com/newsainturtle/shadowmate/kh/user/UserControllerTest.java
+++ BE/src/test/java/com/newsainturtle/shadowmate/kh/user/UserControllerTest.java
@@ -1,10 +1,14 @@
 package com.newsainturtle.shadowmate.kh.user;
 
 import com.google.gson.Gson;
+import com.newsainturtle.shadowmate.auth.exception.AuthErrorResult;
+import com.newsainturtle.shadowmate.auth.exception.AuthException;
 import com.newsainturtle.shadowmate.auth.service.AuthServiceImpl;
 import com.newsainturtle.shadowmate.common.GlobalExceptionHandler;
 import com.newsainturtle.shadowmate.follow.enums.FollowStatus;
+import com.newsainturtle.shadowmate.user.constant.UserConstant;
 import com.newsainturtle.shadowmate.user.controller.UserController;
+import com.newsainturtle.shadowmate.user.dto.UpdateProfileImageRequest;
 import com.newsainturtle.shadowmate.user.dto.UserResponse;
 import com.newsainturtle.shadowmate.user.entity.User;
 import com.newsainturtle.shadowmate.user.enums.PlannerAccessScope;
@@ -19,14 +23,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static com.newsainturtle.shadowmate.user.constant.UserConstant.SUCCESS_UPDATE_PROFILEIMAGE;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -102,6 +109,46 @@ public class UserControllerTest {
             // then
             resultActions.andExpect(status().isOk());
         }
+
+        @Test
+        void 실패_프로필사진수정_유저틀림() throws Exception {
+            //given
+            final String url = "/api/users/{userId}/images";
+            final UpdateProfileImageRequest updateProfileImageRequest = UpdateProfileImageRequest.builder()
+                    .newProfileImage("NewProfileImage")
+                    .build();
+            doThrow(new AuthException(AuthErrorResult.UNREGISTERED_USER)).when(authService).certifyUser(any(), any());
+
+            //when
+            final ResultActions resultActions = mockMvc.perform(
+                    MockMvcRequestBuilders.put(url,userId)
+                            .content(gson.toJson(updateProfileImageRequest))
+                            .contentType(MediaType.APPLICATION_JSON));
+
+            //then
+            resultActions.andExpect(status().isForbidden());
+        }
+
+
+        @Test
+        void 성공_프로필사진수정() throws Exception {
+            //given
+            final String url = "/api/users/{userId}/images";
+            final UpdateProfileImageRequest updateProfileImageRequest = UpdateProfileImageRequest.builder()
+                    .newProfileImage("NewProfileImage")
+                    .build();
+
+            //when
+            final ResultActions resultActions = mockMvc.perform(
+                    MockMvcRequestBuilders.put(url,userId)
+                            .content(gson.toJson(updateProfileImageRequest))
+                            .contentType(MediaType.APPLICATION_JSON));
+
+            //then
+            resultActions.andExpect(status().isOk())
+                    .andExpect(jsonPath("$.message").value(SUCCESS_UPDATE_PROFILEIMAGE));
+        }
+
     }
 
     @Nested
