b9ac5b3c18c81b77705dd1073b58841447145df0
taboowiths <tabou9820@gmail.com>
2023-10-19 16:46:30 +0900

    [FE] refactor: 주간 플래너 조회 임의값으로 설정 및 저장

--- FE/src/pages/Planner/WeekPage.tsx
+++ FE/src/pages/Planner/WeekPage.tsx
@@ -1,8 +1,24 @@
 import Week from "@components/planner/week/Week";
 import WeekItem from "@components/planner/week/WeekItem";
-import React from "react";
+import { useAppDispatch } from "@hooks/hook";
+import { setWeekInfo } from "@store/weekSlice";
+import { TODO_ITEMS_RESPONSE } from "@util/data/WeekTodos";
+import React, { useEffect } from "react";
 
 const WeekPage = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    const response = TODO_ITEMS_RESPONSE;
+    dispatch(
+      setWeekInfo({
+        plannerAccessScope: response.data.plannerAccessScope,
+        dday: response.data.dday,
+        weeklyTodo: response.data.weeklyTodo,
+        dayList: response.data.dayList,
+      }),
+    );
+  }, []);
+
   return <Week />;
 };
 
