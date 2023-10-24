65fa4380742408a72da0b3d9af3dd7d87af8acba
taboowiths <tabou9820@gmail.com>
2023-10-24 23:43:37 +0900

    [FE] feat: 할일 목록 주별&일별 공통 인터페이스 생성

--- FE/src/util/planner.interface.ts
+++ FE/src/util/planner.interface.ts
@@ -13,19 +13,28 @@ export interface todoType {
   todoStatus: 0 | 1 | 2; // none, O, X
 }
 
+// 공통 Todo Item
+export interface TodoConfig {
+  todoId: number;
+  category?: categoryType;
+  todoContent: string;
+  todoStatus: string;
+  todoUpdate?: boolean;
+  timeTable?: TimeTableConfig;
+}
+
+export interface TimeTableConfig {
+  timeTableId: number;
+  startTime: string;
+  endTime: string;
+}
+
 /* --- Week Interfaces --- */
 
 export interface WeekTodoItemConfig {
   weeklyTodoContent: string;
   weeklyTodoStatus: boolean;
   weeklyTodoUpdate: boolean;
-}
-
-export interface TodoItemConfig {
-  todoContents: string;
-  todoStatus: boolean;
-  categoryEmoticon?: string;
-  todoUpdate: boolean;
 }
 
 export interface ddayType {
