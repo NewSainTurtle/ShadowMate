449ce1d941c890027bda62d09b9697bfc21a69b0
dustnzlzl <dustnzlzl@naver.com>
2023-10-28 18:17:47 +0900

    [FE] feat: 회원 가입 API 연결

--- FE/src/components/auth/signup/Signup.tsx
+++ FE/src/components/auth/signup/Signup.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import styles from "@styles/auth/Signup.module.scss";
 import AuthButton from "@components/auth/AuthButton";
 import Text from "@components/common/Text";
@@ -9,6 +9,7 @@ import { authApi } from "@api/Api";
 import { userRegex, removeWhitespaceRegex } from "@util/regex";
 
 const Signup = () => {
+  const navigator = useNavigate();
   const [userData, setUserData] = useState({
     email: "",
     code: "",
@@ -16,25 +17,70 @@ const Signup = () => {
     passwordCheck: "",
     nickname: "",
   });
-  const [isCheck, setCheck] = useState({
-    email: false, // 이메일 형식, 이메일 인증 요청 여부
-    emailCheck: false, // 이메일 인증 확인 여부
-    passwordSame: false, // 패스워드 형식, 일치 여부
-    nicknameCheck: false, // 닉네임 형식, 중복 여부
-  });
+  const [isEmailAuthentication, setEmailAuthentication] = useState(false);
+  const [isEmailRedundancy, setEmailRedundancy] = useState(false);
+  const [isNickanmeAuthentication, setNickanmeAuthentication] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const { email, code, password, passwordCheck, nickname } = userData;
 
-  const EmailRe = () => {
-    console.log(userRegex.email.test(email));
-  };
-
   const signupApiModule = (() => {
-    const onClickEmail = () =>
-      userRegex.email.test(email) ? authApi.emailAuthentication({ email }) : setErrorMessage("이메일 형식이 아닙니다");
-    const onClickEmailCheck = () => authApi.emailAuthenticationCheck({ email, code });
-    const onClickNickName = () => authApi.nickname({ nickname });
-    const onClickJoin = () => authApi.join({ email, password, nickname });
+    const onClickEmail = () => {
+      if (userRegex.email.test(email))
+        authApi
+          .emailAuthentication({ email })
+          .then(() => {
+            setEmailAuthentication(true);
+            setEmailRedundancy(false);
+            setErrorMessage("");
+          })
+          .catch(() => {
+            setEmailAuthentication(false);
+            setErrorMessage("중복된 이메일 입니다.");
+          });
+      else setErrorMessage("이메일 형식이 아닙니다");
+    };
+    const onClickEmailCheck = () =>
+      authApi
+        .emailAuthenticationCheck({ email, code })
+        .then((res) => {
+          console.log(res);
+          setEmailRedundancy(true);
+          setErrorMessage("");
+        })
+        .catch(() => {
+          setEmailRedundancy(false);
+          setErrorMessage("이메일 인증 코드가 일치하지 않습니다");
+        });
+    const onClickNickName = () => {
+      if (userRegex.nickname.test(nickname)) {
+        setNickanmeAuthentication(true);
+        setErrorMessage("");
+        //백 API 수정 필요
+        // authApi
+        //   .nickname({ nickname })
+        //   .then(() => {
+        //     setNickanmeAuthentication(true);
+        //     setErrorMessage("");
+        //   })
+        //   .catch((err) => {
+        //     setNickanmeAuthentication(false);
+        //     setErrorMessage("중복된 닉네임 입니다");
+        //   });
+      } else {
+        setNickanmeAuthentication(false);
+        setErrorMessage("닉네임은 2 ~ 10글자이내로 입력해주세요");
+      }
+    };
+    const onClickJoin = () => {
+      if (userRegex.password.test(password)) setErrorMessage("비밀번호는 6 ~ 20이내로 입력해주세요");
+      else if (password != passwordCheck) setErrorMessage("비밀번호가 일치하지 않습니다.");
+      else if (!isEmailRedundancy) setErrorMessage("이메일을 인증 해주세요");
+      else if (!isNickanmeAuthentication) setErrorMessage("닉네임을 인증 해주세요");
+      else if (!!email && !!password && !!nickname) {
+        authApi.join({ email, password, nickname });
+        navigator("/login");
+      }
+    };
 
     return {
       onClickEmail,
@@ -52,22 +98,6 @@ const Signup = () => {
     });
   };
 
-  // const isEmail = userRegex.email.test(email) ? setErrorMessage("이메일 형식이 아닙니다") : "";
-  const isEmailAuthentication = false && setErrorMessage("이메일 인증을 해주세요");
-  const isEmailRedundancy = false && setErrorMessage("중복된 이메일 입니다");
-  const isNickname = userRegex.nickname.test(nickname) && setErrorMessage("닉네임은 2 ~ 10글자");
-  const isNickanmeAuthentication = false && setErrorMessage("닉네임을 인증해주세요");
-  const isNickNameRedundancy = false && setErrorMessage("중복된 닉네임 입니다");
-  const passwordRex = userRegex.password.test(password) && setErrorMessage("비밀번호는 6 ~ 20글자 가능합니다.");
-  // const isPasswordSame = password === passwordCheck && setErrorMessage("비밀번호가 일치하지 않습니다.");
-  const isError =
-    isEmailAuthentication &&
-    isEmailRedundancy &&
-    isNickname &&
-    isNickanmeAuthentication &&
-    isNickNameRedundancy &&
-    passwordRex;
-
   return (
     <div className={styles["signup_display"]}>
       <div className={styles["signup_container"]}>
@@ -77,15 +107,17 @@ const Signup = () => {
           <div className={styles["input_box__button"]}>
             <Input placeholder="이메일" name="email" value={email} onChange={handleInput} />
             <Button types="gray" onClick={onClickEmail}>
-              인증
+              {!isEmailAuthentication ? "인증" : "재전송"}
             </Button>
           </div>
-          <div className={styles["input_box__button"]}>
-            <Input placeholder="이메일 인증 코드" name="emailCode" value={code} onChange={handleInput} />
-            <Button types="gray" onClick={onClickEmailCheck}>
-              확인
-            </Button>
-          </div>
+          {isEmailAuthentication && (
+            <div className={styles["input_box__button"]}>
+              <Input placeholder="이메일 인증 코드" name="code" value={code} onChange={handleInput} />
+              <Button types="gray" onClick={onClickEmailCheck} disabled={isEmailRedundancy}>
+                {!isEmailRedundancy ? "인증확인" : "인증완료"}
+              </Button>
+            </div>
+          )}
           <Input placeholder="비밀번호" types="password" name="password" value={password} onChange={handleInput} />
           <Input
             placeholder="비밀번호 확인"
@@ -96,14 +128,14 @@ const Signup = () => {
           />
           <div className={styles["input_box__button"]}>
             <Input placeholder="닉네임" name="nickname" value={nickname} onChange={handleInput} />
-            <Button types="gray" onClick={onClickNickName}>
-              중복검사
+            <Button types="gray" onClick={onClickNickName} disabled={isNickanmeAuthentication}>
+              {!isNickanmeAuthentication ? "중복검사" : "검사완료"}
             </Button>
           </div>
         </div>
 
         <div className={styles["signup_box_2"]}>
-          <div className={styles["signup_warning"]} style={{ visibility: "ㅇ" ? "visible" : "hidden" }}>
+          <div className={styles["signup_warning"]}>
             <Text types="small">{errorMessage}</Text>
           </div>
           <div onClick={onClickJoin}>
