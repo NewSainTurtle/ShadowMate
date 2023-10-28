8883448257ba0bf40a89d11e25c4f6c2cadbb290
taboowiths <tabou9820@gmail.com>
2023-10-28 22:29:00 +0900

    [FE] feat: redux-persist 설정

--- FE/src/hooks/configStore.ts
+++ FE/src/hooks/configStore.ts
@@ -2,6 +2,9 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "@store/authSlice";
 import weekReducer from "@store/planner/weekSlice";
 import dayReducer from "@store/planner/daySlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
 
 const rootReducer = combineReducers({
   auth: authReducer,
@@ -9,13 +12,21 @@ const rootReducer = combineReducers({
   day: dayReducer,
 });
 
+const persistConfig = {
+  key: "root",
+  whitelist: ["auth"],
+  storage,
+};
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistReducer(persistConfig, rootReducer),
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type rootState = ReturnType<typeof store.getState>;
