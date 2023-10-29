608001c1eda56fc12e7fcbb946ebe4a48e9701af
taboowiths <tabou9820@gmail.com>
2023-10-29 22:35:46 +0900

    [FE] modify: 불필요한 코드 및 파일 삭제

--- FE/src/pages/Planner/WeekPage.tsx
+++ FE/src/pages/Planner/WeekPage.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from "react";
 import Week from "@components/planner/week/Week";
-import { useAppDispatch } from "@hooks/hook";
-import { setWeekInfo } from "@store/planner/weekSlice";
-import { TODO_ITEMS_RESPONSE } from "@util/data/WeekTodos";
 
 const WeekPage = () => {
   return <Week />;
