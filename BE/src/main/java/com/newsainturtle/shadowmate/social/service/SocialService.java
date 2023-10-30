65ea427d90b35c81062c55b77065cce00d77abb5
tgb02087 <tgb02087@naver.com>
2023-10-30 17:56:00 +0900

    [BE] feat: 공개된 소셜 플래너 Service API 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/social/service/SocialService.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/social/service/SocialService.java
@@ -1,9 +1,6 @@
 package com.newsainturtle.shadowmate.social.service;
 
 import com.newsainturtle.shadowmate.social.dto.SearchPublicDailyPlannerResponse;
-import com.newsainturtle.shadowmate.social.entity.Social;
-
-import java.util.List;
 
 public interface SocialService {
 
