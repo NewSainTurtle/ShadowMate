f35ab8ebcee6a8cb651a1cdf0dab5da0dff235e4
yunaghgh <yunaghgh@naver.com>
2023-10-30 21:50:17 +0900

    [BE] fix: 충돌해결로 삭제된 일일플래너 패키지 import

--- BE/src/main/java/com/newsainturtle/shadowmate/social/repository/SocialRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/social/repository/SocialRepository.java
@@ -1,5 +1,6 @@
 package com.newsainturtle.shadowmate.social.repository;
 
+import com.newsainturtle.shadowmate.planner.entity.DailyPlanner;
 import com.newsainturtle.shadowmate.social.entity.Social;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
