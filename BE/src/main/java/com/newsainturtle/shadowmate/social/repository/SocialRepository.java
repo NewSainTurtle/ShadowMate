077af61aee5693e90260b53585f18f4e186a5242
yunaghgh <yunaghgh@naver.com>
2023-10-30 22:27:14 +0900

    [BE] test: 소셜 숨기기/보이기 레포지토리 테스트 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/social/repository/SocialRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/social/repository/SocialRepository.java
@@ -2,10 +2,13 @@ package com.newsainturtle.shadowmate.social.repository;
 
 import com.newsainturtle.shadowmate.planner.entity.DailyPlanner;
 import com.newsainturtle.shadowmate.social.entity.Social;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -13,6 +16,10 @@ public interface SocialRepository extends JpaRepository<Social, Long> {
 
     @Query(value = "SELECT s FROM Social s WHERE s.deleteTime is null")
     List<Social> findAllByDeleteTime();
-  
+
     Social findByDailyPlanner(final DailyPlanner dailyPlanner);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Social s set s.deleteTime = :time where s.dailyPlanner in (:dailyPlanners)")
+    void updateDeleteTimeAll(@Param("time") final LocalDateTime time, @Param("dailyPlanners") final List<DailyPlanner> dailyPlanners);
 }
