4f032db7ec1710738cbcb316732c2c9f75ab73f4
taboowiths <tabou9820@gmail.com>
2023-10-31 16:02:13 +0900

    [FE] feat: 캘린더 조회 API 연결

--- FE/src/components/planner/month/Month.tsx
+++ FE/src/components/planner/month/Month.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
 import styles from "@styles/planner/Month.module.scss";
-import MonthCalendar from "./MonthCalendar";
-import MonthFriends from "./MonthFriends";
+import MonthCalendar from "@components/planner/month/MonthCalendar";
+import MonthFriends from "@components/planner/month/MonthFriends";
 import Text from "@components/common/Text";
 import dayjs from "dayjs";
+import { useAppDispatch, useAppSelector } from "@hooks/hook";
+import { selectUserId } from "@store/authSlice";
+import { MonthDayConfig, setMonthInfo } from "@store/planner/monthSlice";
+import { plannerApi } from "@api/Api";
 
 const Month = () => {
+  const dispatch = useAppDispatch();
   const today = new Date();
   const [selectedDay, setSelectedDay] = useState<string>(dayjs(today).format("YYYY-MM-DD"));
   const year: number = dayjs(selectedDay).year();
   const month: number = dayjs(selectedDay).month() + 1;
+  const userId = useAppSelector(selectUserId);
 
   const handlePrevMonth = () => {
     const newDate = dayjs(selectedDay).subtract(1, "month").endOf("month").format("MM/DD/YY");
@@ -20,6 +26,21 @@ const Month = () => {
     const newDate = dayjs(selectedDay).add(1, "month").startOf("month").format("MM/DD/YY");
     setSelectedDay(newDate);
   };
+
+  const getMonthInfo = () => {
+    plannerApi
+      .calendars(userId, { date: dayjs(new Date(year, month - 1, 1)).format("YYYY-MM-DD") })
+      .then((res) => {
+        const dayList: MonthDayConfig[] = res.data.data.dayList;
+        const plannerAccessScope: string = res.data.data.plannerAcessScope || "전체공개";
+        dispatch(setMonthInfo({ plannerAccessScope, dayList }));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getMonthInfo();
+  }, [selectedDay]);
 
   return (
     <div className={styles["month"]}>
