a4008ffc0ce5f936513fd06eeb1edb0033e68fa1
dustnzlzl <dustnzlzl@naver.com>
2023-10-31 10:08:16 +0900

    [FE] feat: 일별 플래너 조회 API 연결

--- FE/src/pages/Planner/DayPage.tsx
+++ FE/src/pages/Planner/DayPage.tsx
@@ -1,51 +1,72 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "@components/planner/day/Header";
 import styles from "@styles/planner/day.module.scss";
 import TimeTable from "@components/planner/day/todo/TimeTable";
 import TodoList from "@components/planner/day/todo/TodoList";
 import Ment from "@components/planner/day/Ment";
-import { useAppSelector } from "@hooks/hook";
-import { selectTodoList } from "@store/planner/daySlice";
+import { useAppDispatch, useAppSelector } from "@hooks/hook";
+import { selectDate, selectTodoList, setDayInfo } from "@store/planner/daySlice";
 import CustomCursor from "@components/planner/day/CustomCursor";
 import dayjs from "dayjs";
 import { TodoConfig } from "@util/planner.interface";
+import { plannerApi } from "@api/Api";
+import { selectUserId } from "@store/authSlice";
 
 const DayPage = () => {
+  const dispatch = useAppDispatch();
+  const userId = useAppSelector(selectUserId);
+  const date = useAppSelector(selectDate);
+  const todoList = useAppSelector(selectTodoList);
   const [ment, setMent] = useState({
     todayGoals: "",
     tomorrowGoals: "",
     retrospections: "",
   });
-  const todoListData = useAppSelector(selectTodoList);
-  const totalTime = useMemo(() => {
-    const sumMinute = todoListData
+  const [isClickTimeTable, setIsClickTimeTable] = useState(false);
+  const todoDivRef = useRef<HTMLDivElement>(null);
+  const [totalTime, setTotalTime] = useState({
+    studyTimeHour: 0,
+    studyTimeMinute: 0,
+  });
+
+  useEffect(() => {
+    const day = dayjs(date).format("YYYY-MM-DD");
+    plannerApi
+      .daily(userId, { date: day })
+      .then((res) => {
+        const response = res.data.data;
+        dispatch(
+          setDayInfo({
+            plannerAccessScope: response.plannerAccessScope,
+            dday: response.dday || "",
+            like: response.like,
+            likeCount: response.likeCount,
+            retrospection: response.retrospection || "",
+            retrospectionImage: response.retrospectionImage || "",
+            todayGoal: response.todayGoal || "",
+            tomorrowGoal: response.tomorrowGoal || "",
+            dailyTodos: response.dailyTodos || [],
+          }),
+        );
+        setTotalTime({ studyTimeHour: response.studyTimeHour, studyTimeMinute: response.studyTimeMinute });
+      })
+      .catch((err) => console.error(err));
+  }, [date]);
+
+  useEffect(() => {
+    const sumMinute = todoList
       .filter((ele: TodoConfig) => ele.timeTable!.startTime != "" && ele.timeTable!.endTime != "")
       .reduce(
         (accumulator: number, item: { timeTable: any }) =>
           accumulator + Number(dayjs(item.timeTable!.endTime).diff(dayjs(item.timeTable!.startTime), "m")),
         0,
       );
+    const studyTimeHour = Math.floor(sumMinute / 60);
+    const studyTimeMinute = Math.floor(sumMinute % 60);
+    setTotalTime({ studyTimeHour, studyTimeMinute });
+  }, [todoList]);
 
-    return {
-      studyTimeHour: Math.floor(sumMinute / 60),
-      studyTimeMinute: Math.floor(sumMinute % 60),
-    };
-  }, [todoListData]);
-  const [isClickTimeTable, setIsClickTimeTable] = useState(false);
-  const todoDivRef = useRef<HTMLDivElement>(null);
-
-  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMent({
-      ...ment,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleClickTimeTable = (props: boolean) => {
-    setIsClickTimeTable(props);
-  };
-
-  useEffect(() => {
+  (() => {
     const handleOutsideClose = (e: MouseEvent) => {
       if (todoDivRef && todoDivRef.current) {
         if (e.button == 2) handleClickTimeTable(false);
@@ -54,7 +75,11 @@ const DayPage = () => {
     document.addEventListener("mouseup", handleOutsideClose);
     document.addEventListener("contextmenu", (e) => e.preventDefault());
     return () => document.addEventListener("mouseup", handleOutsideClose);
-  }, []);
+  })();
+
+  const handleClickTimeTable = (props: boolean) => {
+    setIsClickTimeTable(props);
+  };
 
   useEffect(() => {
     const handleOutsideClose = (e: { target: any }) => {
@@ -64,11 +89,18 @@ const DayPage = () => {
     return () => document.removeEventListener("click", handleOutsideClose);
   }, [isClickTimeTable]);
 
+  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMent({
+      ...ment,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const { todayGoals, tomorrowGoals, retrospections } = ment;
   const { studyTimeHour, studyTimeMinute } = totalTime;
 
   return (
-    <div className={styles["page-container"]}>
+    <div className={styles["page-container"]} key={date}>
       <Header />
       <div className={styles["page-content"]}>
         <Ment
