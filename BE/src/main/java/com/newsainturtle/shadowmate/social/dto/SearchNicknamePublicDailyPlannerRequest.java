b85c1eb00fcfded8e95b500948cc91c2b821bbed
tgb02087 <tgb02087@naver.com>
2023-11-02 15:23:58 +0900

    [BE] refactor: @Valid 유효성 검사 어노테이션 추가

--- BE/src/main/java/com/newsainturtle/shadowmate/social/dto/SearchNicknamePublicDailyPlannerRequest.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/social/dto/SearchNicknamePublicDailyPlannerRequest.java
@@ -5,14 +5,23 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SearchNicknamePublicDailyPlannerRequest {
 
+    @NotNull
+    @Size(min = 2, max = 10)
     private String nickname;
 
+    @NotNull
     private String sort;
 
+    @NotNull
+    @Positive
     private long pageNumber;
 
     @Builder
