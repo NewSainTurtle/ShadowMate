785921fb3c7a15802a1b44b574b91d6cb197bb05
tgb02087 <tgb02087@naver.com>
2023-11-02 14:42:32 +0900

    [BE] feat: 내정보 수정 Repository API 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/user/repository/UserRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/user/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.newsainturtle.shadowmate.user.repository;
 import com.newsainturtle.shadowmate.user.entity.User;
 import com.newsainturtle.shadowmate.user.enums.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(final String email);
@@ -12,4 +15,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmailAndSocialLogin(final String email, final SocialType socialType);
 
     User findByIdAndWithdrawalIsFalse(final Long userId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update User u set u.nickname = :nickname, u.profileImage = :profileImage, u.statusMessage = :statusMessage where  u.id = :userId")
+    void updateUser(@Param("nickname") final String nickname, @Param("profileImage") final String profileImage, @Param("statusMessage") final String statusMessage, @Param("userId") final long userId);
 }
