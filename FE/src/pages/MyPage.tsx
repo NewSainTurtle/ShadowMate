632dda7b7d9b77cd066d8c1c9a80d6501470fc49
taboowiths <tabou9820@gmail.com>
2023-11-02 18:06:44 +0900

    [FE] modify: 카테고리, 디데이 목록 조회 API 연결

--- FE/src/pages/MyPage.tsx
+++ FE/src/pages/MyPage.tsx
@@ -10,7 +10,7 @@ import MyFriend from "@components/mypage/details/friend/MyFriend";
 import { settingApi } from "@api/Api";
 import { useAppDispatch, useAppSelector } from "@hooks/hook";
 import { selectUserId } from "@store/authSlice";
-import { setCategoryColors, setCategoryInput, setCategoryList } from "@store/mypageSlice";
+import { setCategoryColors, setCategoryInput, setCategoryList, setDdayList } from "@store/mypageSlice";
 import { CategoryConfig } from "@util/planner.interface";
 
 const MyPage = () => {
@@ -43,7 +43,18 @@ const MyPage = () => {
       });
   };
 
+  const getDdayList = () => {
+    settingApi
+      .ddays(userId)
+      .then((res) => {
+        const response = res.data.data.ddayList;
+        dispatch(setDdayList(response));
+      })
+      .catch((err) => console.log(err));
+  };
+
   useEffect(() => {
+    getDdayList();
     getCategoryList();
     getCategoryColors();
   }, []);
