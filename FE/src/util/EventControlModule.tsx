08bde74a2e8f9ac618d424aa0dbc1a9c06bfd8ce
dustnzlzl <dustnzlzl@naver.com>
2023-11-02 23:24:41 +0900

    [FE] feat: 상태 디바운싱 모듈 추가

--- FE/src/util/EventControlModule.tsx
+++ FE/src/util/EventControlModule.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce(value: string, delay = 1000) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value]);
+
+  return debouncedValue;
+}
+
 export const debouncing = <T extends (...args: any[]) => any>(fn: T, delay: number) => {
   let timeId: ReturnType<typeof setTimeout>;
   return (...args: Parameters<T>): ReturnType<T> => {
