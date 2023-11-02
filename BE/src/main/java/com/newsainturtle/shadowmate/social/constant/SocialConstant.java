84244da99589b582dfb91068dc698ff0d01b61dd
tgb02087 <tgb02087@naver.com>
2023-11-02 13:45:35 +0900

    [BE] feat: 공개플래너 닉네임검색 Constant API 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/social/constant/SocialConstant.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/social/constant/SocialConstant.java
@@ -7,4 +7,5 @@ import lombok.NoArgsConstructor;
 public class SocialConstant {
 
     public static final String SUCCESS_SEARCH_PUBLIC_DAILY_PLANNER = "공개된 소셜 플래너 조회가 완료되었습니다.";
+    public static final String SUCCESS_SEARCH_NICKNAME_PUBLIC_DAILY_PLANNER = "공개된 소셜 플래너 닉네임 조회가 완료되었습니다.";
 }
