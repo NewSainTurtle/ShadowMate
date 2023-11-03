d53dda7e43cf447591cce137aa9e48b30248968c
dustnzlzl <dustnzlzl@naver.com>
2023-11-04 07:02:22 +0900

    [FE] feat: 내 정보 수정에서 닉네임 및 상태 메세지 유효성 검사

--- FE/src/components/mypage/details/myInfo/MyInfo.tsx
+++ FE/src/components/mypage/details/myInfo/MyInfo.tsx
@@ -10,20 +10,63 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { firebaseStorage } from "@api/firebaseConfig";
 import { useAppDispatch, useAppSelector } from "@hooks/hook";
 import { userApi } from "@api/Api";
+import { userRegex } from "@util/regex";
 
 const MyPageInfo = () => {
   const dispatch = useAppDispatch();
   const userId = useAppSelector(selectUserId);
   const myInfoData: userInfoConfig = useAppSelector(selectUserInfo);
   const [userMyInfo, setUserMyInfo] = useState<userInfoConfig>(myInfoData);
-  const [saveImageFile, setSaveImageFile] = useState<File | null>(null);
   const { email, nickname, profileImage, statusMessage } = userMyInfo;
+  const [saveImageFile, setSaveImageFile] = useState<File | null>(null);
+  const [isNickanmeAuthentication, setNickanmeAuthentication] = useState(myInfoData.nickname == nickname);
+  const [isErrorButton, setErrorButton] = useState(false);
+  const [error, setError] = useState({
+    nickname: false,
+    statusMessage: false,
+  });
+  const [length, setLength] = useState({
+    nickname: nickname.length,
+    statusMessage: statusMessage.length,
+  });
 
   const handleUser = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLength({ ...length, [name]: value.length });
+    if (name == "nickname") {
+      if (setErrorButton) setErrorButton(false);
+      if (isNickanmeAuthentication) setNickanmeAuthentication(false);
+      if (myInfoData.nickname == value) setNickanmeAuthentication(true);
+      if (!userRegex.nickname.test(value)) setError({ ...error, [name]: true });
+      else setError({ ...error, [name]: false });
+    } else if (name == "statusMessage") {
+      if (!userRegex.statusMessage.test(value)) setError({ ...error, [name]: true });
+      else setError({ ...error, [name]: false });
+    }
     setUserMyInfo({
       ...userMyInfo,
-      [e.target.name]: e.target.value,
+      [name]: value,
     });
+  };
+
+  const onClickNickName = () => {
+    if (userRegex.nickname.test(nickname)) {
+      setNickanmeAuthentication(true);
+      setErrorButton(false);
+      //백 API 수정 필요
+      // authApi
+      //   .nickname({ nickname })
+      //   .then(() => {
+      //     setNickanmeAuthentication(true);
+      //     setErrorMessage("");
+      //   })
+      //   .catch((err) => {
+      //     setNickanmeAuthentication(false);
+      //     setErrorMessage("중복된 닉네임 입니다");
+      //   });
+    } else {
+      setNickanmeAuthentication(false);
+    }
   };
 
   const deleteImgFile = () => {
@@ -47,21 +90,28 @@ const MyPageInfo = () => {
   };
 
   const saveMyInfo = async () => {
-    let newProfileImage = profileImage;
-    if (saveImageFile != null) {
-      const file = saveImageFile;
-      const storageRef = ref(firebaseStorage, `profile/${file.name}`);
-      await uploadBytes(storageRef, file).then((snapshot) =>
-        getDownloadURL(snapshot.ref).then((downloadURL) => (newProfileImage = downloadURL)),
-      );
+    if (myInfoData == userMyInfo) return;
+    if (!isNickanmeAuthentication) {
+      setErrorButton(true);
+    } else {
+      let newProfileImage = profileImage;
+      if (saveImageFile != null) {
+        const file = saveImageFile;
+        const storageRef = ref(firebaseStorage, `profile/${file.name}`);
+        await uploadBytes(storageRef, file).then((snapshot) =>
+          getDownloadURL(snapshot.ref).then((downloadURL) => (newProfileImage = downloadURL)),
+        );
+      }
+      await userApi
+        .myPages(userId, { newNickname: nickname, newProfileImage, newStatusMessage: statusMessage })
+        .then(() => {
+          dispatch(setUserInfo({ ...userMyInfo, profileImage: newProfileImage }));
+        })
+        .catch((err) => console.error(err));
     }
-    await userApi
-      .myPages(userId, { newNickname: nickname, newProfileImage, newStatusMessage: statusMessage })
-      .then(() => {
-        dispatch(setUserInfo({ ...userMyInfo, profileImage: newProfileImage }));
-      })
-      .catch((err) => console.error(err));
   };
+
+  const errorButtonStyle = isErrorButton ? "--error" : "";
 
   const MY_INFO = (() => {
     return [
@@ -69,15 +119,41 @@ const MyPageInfo = () => {
       {
         title: "닉네임",
         node: (
-          <div className={styles["info__profile-nickname"]}>
-            <Input placeholder="닉네임" name="nickname" value={nickname} onChange={handleUser} />
-            <Button types="gray">중복검사</Button>
+          <div className={`${styles["info__profile-nickname"]} ${styles[errorButtonStyle]}`}>
+            <Input
+              placeholder="닉네임"
+              name="nickname"
+              value={nickname}
+              onChange={handleUser}
+              error={error.nickname}
+              helperText={
+                error.nickname
+                  ? "공백을 제외한 2 ~ 10자의 닉네임을 입력할 수 있습니다."
+                  : `글자 수: ${length.nickname}/10`
+              }
+            />
+            <Button types="gray" onClick={onClickNickName} disabled={isNickanmeAuthentication}>
+              중복검사
+            </Button>
           </div>
         ),
       },
       {
         title: "상태 메세지",
-        node: <Input placeholder="상태 메세지 입력" name="statusMessage" value={statusMessage} onChange={handleUser} />,
+        node: (
+          <Input
+            placeholder="상태 메세지 입력"
+            name="statusMessage"
+            value={statusMessage}
+            onChange={handleUser}
+            error={error.statusMessage}
+            helperText={
+              error.statusMessage
+                ? "0 ~ 20자의 상태 메세지를 입력할 수 있습니다."
+                : `글자 수: ${length.statusMessage}/20`
+            }
+          />
+        ),
       },
       {
         title: "프로필 사진",
