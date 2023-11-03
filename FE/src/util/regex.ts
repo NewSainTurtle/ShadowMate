ffdea4b19bbc9e6fe8c95b9ae95bd9a27d55d4f3
dustnzlzl <dustnzlzl@naver.com>
2023-11-04 07:06:06 +0900

    [FE] feat: 상태메세지 정규표현식 추가

--- FE/src/util/regex.ts
+++ FE/src/util/regex.ts
@@ -1,8 +1,8 @@
 export const userRegex = {
   email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/,
-  password: /^[a-zA-Z\\d!?^&*@#]{6,20}$/,
-  nickname: /^[ㄱ-ㅎ가-힣a-zA-Z\\d]{2,10}$/,
-  statusMessage: /^[ㄱ-ㅎ가-힣a-zA-Z\\d]{0,20}$/,
+  password: /^[\w!?^&*@#]{6,20}$/,
+  nickname: /^[ㄱ-ㅎ가-힣\w]{2,10}$/,
+  statusMessage: /^[ㄱ-ㅎ가-힣\w\s]{0,20}$/,
 };
 
 export const removeWhitespaceRegex = /\s/g;
