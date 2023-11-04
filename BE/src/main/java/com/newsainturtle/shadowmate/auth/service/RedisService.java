d7db17cd771c8577bfb4f2b417e9a6a657d224d6
tgb02087 <tgb02087@naver.com>
2023-11-04 18:41:07 +0900

    [BE] feat: 닉네임 중복검사 Redis API 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/auth/service/RedisService.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/auth/service/RedisService.java
@@ -5,7 +5,10 @@ import com.newsainturtle.shadowmate.auth.entity.EmailAuthentication;
 public interface RedisService {
 
     EmailAuthentication getHashEmailData(final String key);
+    Boolean getHashNicknameData(final String key);
     void setHashEmailData(final String key, final EmailAuthentication value, final int timeout);
+    void setHashNicknameData(final String key, final boolean value, final int timeout);
     void deleteEmailData(final String key);
+    void deleteNicknameData(final String key);
 
 }
