e802747389813f5722df96935dda4ed527532068
taboowiths <tabou9820@gmail.com>
2023-11-05 15:09:51 +0900

    [FE] design: 공통 Alert 컴포넌트 디자인 적용

--- FE/src/components/common/Alert/index.tsx
+++ FE/src/components/common/Alert/index.tsx
@@ -1,23 +1,25 @@
 import React from "react";
+import styles from "@styles/common/Alert.module.scss";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 
 interface Props {
+  types: "success" | "error";
   open: boolean;
   onClose: () => void;
   message: string;
 }
 
-const Alert = ({ open, onClose, message, ...rest }: Props) => {
+const Alert = ({ types, open, onClose, message, ...rest }: Props) => {
   return (
     <Snackbar
-      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
       open={open}
       onClose={onClose}
       autoHideDuration={2000}
     >
-      <MuiAlert onClose={onClose} severity="success" sx={{ width: "100%" }}>
-        {message}
+      <MuiAlert icon={false} className={styles[`alert__${types}`]} onClose={onClose}>
+        {{ success: "저장되었습니다.", error: "저장에 실패했습니다. 다시 시도해주세요." }[types]}
       </MuiAlert>
     </Snackbar>
   );
