3bb6166716323262f8a2ff3e44ce6097df697bb6
dustnzlzl <dustnzlzl@naver.com>
2023-11-07 00:13:21 +0900

    [FE] feat: 소셜 조회 및 닉네임 검색 API 연결

--- FE/src/components/social/CardList.tsx
+++ FE/src/components/social/CardList.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "@styles/social/Social.module.scss";
 import CardItem from "@components/social/CardItem";
 import { ProfileConfig } from "@components/common/FriendProfile";
@@ -12,26 +12,28 @@ export interface SocialListType extends ProfileConfig {
   dailyPlannerDay: string;
 }
 
-const CardList = ({ order, search }: { order: string; search: string }) => {
+const CardList = ({ sort, search: nickname }: { sort: "latest" | "popularity"; search: string }) => {
   const userId = useAppSelector(selectUserId);
-  const [List, setList] = useState<SocialListType[]>([]);
+  const [list, setList] = useState<SocialListType[]>([]);
   const [pageNumber, setPageNumber] = useState(1);
-
   const preventRef = useRef(true);
-  const obsRef = useRef(null);
   const endRef = useRef(false);
+  const obsRef = useRef(null);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(obsHandler, { threshold: 0.5 });
+    preventRef.current = false;
+    endRef.current = false;
+    setPageNumber(1);
+    getPost();
+  }, [sort, nickname.length]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(obsHandler);
     if (obsRef.current) observer.observe(obsRef.current);
     return () => observer.disconnect();
   }, []);
 
-  useEffect(() => {
-    getPost();
-  }, [pageNumber]);
-
-  const obsHandler = (entries: any[]) => {
+  const obsHandler = async (entries: any[]) => {
     const target = entries[0];
     if (!endRef.current && target.isIntersecting && preventRef.current) {
       preventRef.current = false;
@@ -39,26 +41,45 @@ const CardList = ({ order, search }: { order: string; search: string }) => {
     }
   };
 
-  const getPost = useCallback(async () => {
-    const sort = order == "최신순" ? "latest" : "popularity";
-
-    socialApi
-      .getSocial(userId, { sort, pageNumber })
-      .then((res) => {
-        const response = res.data.data;
-        if (pageNumber == response.totalPage) endRef.current = true;
-        setList((prev) => [...prev, ...response.socialList]);
-        preventRef.current = true;
-      })
-      .catch((err) => console.error(err));
+  useEffect(() => {
+    if (pageNumber != 1) getPost();
   }, [pageNumber]);
+
+  const getPost = async () => {
+    if (nickname.length >= 2) {
+      socialApi
+        .searches(userId, { nickname, sort, pageNumber })
+        .then((res) => {
+          const response = res.data.data;
+          if (pageNumber == response.totalPage) endRef.current = true;
+          if (pageNumber == 1) setList(response.socialList);
+          else setList((prev) => [...prev, ...response.socialList]);
+          preventRef.current = true;
+        })
+        .catch((err) => console.error(err));
+    } else {
+      socialApi
+        .getSocial(userId, { sort, pageNumber })
+        .then((res) => {
+          const response = res.data.data;
+          if (pageNumber == response.totalPage) endRef.current = true;
+          if (pageNumber == 1) setList(response.socialList);
+          else setList((prev) => [...prev, ...response.socialList]);
+          preventRef.current = true;
+        })
+        .catch((err) => console.error(err));
+    }
+  };
 
   return (
     <div className={styles["card-list"]}>
-      {List.map((social) => (
-        <CardItem key={social.socialId} item={social} />
+      {list.map((social, idx) => (
+        <CardItem key={social.socialId + idx} item={social} />
       ))}
-      <div ref={obsRef} />
+
+      {!list.length && <span>검색된 결과가 존재하지 않습니다.</span>}
+
+      <div ref={obsRef} style={{ backgroundColor: "lightpink" }} />
     </div>
   );
 };
