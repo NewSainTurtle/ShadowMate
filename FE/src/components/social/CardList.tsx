8f92cffb9e739bdea1e9d7945cc81ad3cf01a6c0
dustnzlzl <dustnzlzl@naver.com>
2023-11-06 10:13:00 +0900

    [FE] feat: 소셜페이지 무한 스크롤

--- FE/src/components/social/CardList.tsx
+++ FE/src/components/social/CardList.tsx
@@ -1,12 +1,10 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import styles from "@styles/social/Social.module.scss";
 import CardItem from "@components/social/CardItem";
-// import { socialListData } from "@util/data/SocialData";
 import { ProfileConfig } from "@components/common/FriendProfile";
 import { socialApi } from "@api/Api";
 import { useAppSelector } from "@hooks/hook";
 import { selectUserId } from "@store/authSlice";
-import Loading from "@components/common/Loading";
 
 export interface SocialListType extends ProfileConfig {
   socialId: number;
@@ -16,23 +14,21 @@ export interface SocialListType extends ProfileConfig {
 
 const CardList = ({ order, search }: { order: string; search: string }) => {
   const userId = useAppSelector(selectUserId);
-  const obsRef = useRef(null);
   const [List, setList] = useState<SocialListType[]>([]);
   const [pageNumber, setPageNumber] = useState(1);
-  const [load, setLoad] = useState(false);
+
   const preventRef = useRef(true);
+  const obsRef = useRef(null);
   const endRef = useRef(false);
 
   useEffect(() => {
     const observer = new IntersectionObserver(obsHandler, { threshold: 0.5 });
     if (obsRef.current) observer.observe(obsRef.current);
-    return () => {
-      observer.disconnect();
-    };
+    return () => observer.disconnect();
   }, []);
 
   useEffect(() => {
-    if (pageNumber !== 1) getPost();
+    getPost();
   }, [pageNumber]);
 
   const obsHandler = (entries: any[]) => {
@@ -45,7 +41,6 @@ const CardList = ({ order, search }: { order: string; search: string }) => {
 
   const getPost = useCallback(async () => {
     const sort = order == "최신순" ? "latest" : "popularity";
-    setLoad(true);
 
     socialApi
       .getSocial(userId, { sort, pageNumber })
@@ -53,19 +48,18 @@ const CardList = ({ order, search }: { order: string; search: string }) => {
         const response = res.data.data;
         console.log(response);
         if (pageNumber == response.totalPage) endRef.current = true;
-        setList((prev) => [...prev, ...response.socialList]); //리스트 추가
+        setList((prev) => [...prev, ...response.socialList]);
+        preventRef.current = true;
       })
-      .catch((err) => console.error(err))
-      .finally(() => setLoad(false));
+      .catch((err) => console.error(err));
   }, [pageNumber]);
 
   return (
     <div className={styles["card-list"]}>
-      {List.map((social, idx) => (
-        <CardItem key={idx} item={social} />
+      {List.map((social) => (
+        <CardItem key={social.socialId} item={social} />
       ))}
-      {load && <Loading />}
-      <div className="" ref={obsRef} />
+      <div ref={obsRef} style={{ height: "1px" }} />
     </div>
   );
 };
