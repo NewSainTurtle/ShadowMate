30fd1a2196748a69f8d60fd078b90b897ba4b83b
dustnzlzl <dustnzlzl@naver.com>
2023-11-07 23:56:41 +0900

    [FE] feat: 닉네임 중복검사 삭제 API 정의

--- FE/src/api/Api.ts
+++ FE/src/api/Api.ts
@@ -7,6 +7,7 @@ export const authApi = {
   login: (data: { email: string; password: string }) => Axios.post(api.auth.login(), data),
   googleLogin: () => Axios.post(api.auth.googleLogin()),
   nickname: (data: { nickname: string }) => Axios.post(api.auth.nickname(), data),
+  deleteNickname: (data: { nickname: string }) => Axios.delete(api.auth.nickname(), { data }),
   emailAuthentication: (data: { email: string }) => Axios.post(api.auth.emailAuthentication(), data),
   emailAuthenticationCheck: (data: { email: string; code: string }) =>
     Axios.post(api.auth.emailAuthenticationCheck(), data),
