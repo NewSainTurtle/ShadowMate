7585fc9f5fa65fe3a9d53d266db5fd39363110f2
dustnzlzl <dustnzlzl@naver.com>
2023-11-08 03:27:57 +0900

    [FE] feat: 회원탈퇴시 로컬스토리지 저장된 테마 삭제

--- FE/src/components/mypage/details/myInfo/CancelMembership.tsx
+++ FE/src/components/mypage/details/myInfo/CancelMembership.tsx
@@ -24,6 +24,7 @@ const CancelMembership = () => {
       })
       .then(() => {
         persistor.purge(); // 리덕스 초기화
+        localStorage.removeItem("theme");
       })
       .catch((err) => console.error(err));
   };
