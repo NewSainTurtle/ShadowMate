6d8880521f08e6e06f63e986811acff1f36764df
yunaghgh <yunaghgh@naver.com>
2023-11-07 14:36:32 +0900

    [BE] modify: 나에게 온 친구요청 목록 삭제 레포지토리 프로덕션 코드 수정

--- BE/src/main/java/com/newsainturtle/shadowmate/follow/repository/FollowRequestRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/follow/repository/FollowRequestRepository.java
@@ -4,6 +4,8 @@ import com.newsainturtle.shadowmate.follow.entity.FollowRequest;
 import com.newsainturtle.shadowmate.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -16,7 +18,7 @@ public interface FollowRequestRepository extends JpaRepository<FollowRequest, Lo
 
     void deleteByRequesterIdAndReceiverId(final User requester, final User receiver);
 
-    @Modifying
-    @Transactional
-    void deleteAllByReceiverId(final User receiver);
+    @Modifying(clearAutomatically = true)
+    @Query("delete from FollowRequest f where f.receiverId.id = :receiverId")
+    void deleteAllByReceiverId(@Param("receiverId") final long receiverId);
 }
