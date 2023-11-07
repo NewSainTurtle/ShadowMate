461342618976f4cefb63978c1b5762df9689932d
dustnzlzl <dustnzlzl@naver.com>
2023-11-08 02:44:42 +0900

    [FE] feat: 사용자 기본 브라우저 테마로 적용

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -41,7 +41,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", "light");
+    const isDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+    document.documentElement.setAttribute("data-theme", isDarkMode ? "dark" : "light");
   }, []);
 
   useEffect(() => {
