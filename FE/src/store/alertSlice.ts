bc9f4e2b4865bcca6193c7afab99aadf4f35f45e
taboowiths <tabou9820@gmail.com>
2023-11-07 14:07:16 +0900

    [FE] feat: alertSlice 생성

---
+++ FE/src/store/alertSlice.ts
@@ -0,0 +1,35 @@
+import React from "react";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { rootState } from "@hooks/configStore";
+
+interface AlertConfig {
+  type: "success" | "error";
+  message: string;
+  open: boolean;
+}
+
+const initialState: AlertConfig = {
+  type: "success",
+  message: "",
+  open: false,
+};
+
+const alertSlice = createSlice({
+  name: "alert",
+  initialState,
+  reducers: {
+    setAlertOpen: (status, { payload }: PayloadAction<{ type: "success" | "error"; message: string }>) => {
+      status.open = true;
+      status.type = payload.type;
+      status.message = payload.message;
+    },
+    setAlertClose: (status) => {
+      status.open = false;
+    },
+  },
+});
+
+export const { setAlertOpen, setAlertClose } = alertSlice.actions;
+export const selectAlertInfo = (state: rootState) => state.alert;
+
+export default alertSlice.reducer;
