4c9f1d8cbb084a766c277764455eace3971b53a3
dustnzlzl <dustnzlzl@naver.com>
2023-11-08 23:06:09 +0900

    [FE] feat: 마이페이지 닉네임 에러메세지 수정

--- FE/src/components/mypage/details/myInfo/MyInfo.tsx
+++ FE/src/components/mypage/details/myInfo/MyInfo.tsx
@@ -141,7 +141,7 @@ const MyPageInfo = () => {
                   : nicknameErrorMessage
                   ? "중복된 닉네임 입니다."
                   : error.nickname
-                  ? "공백을 제외한 2 ~ 10자의 닉네임을 입력할 수 있습니다."
+                  ? "공백과 특수문자를 제외한 2~10자의 닉네임을 입력할 수 있습니다."
                   : myInfoData.nickname != nickname && isNicknameAuthentication
                   ? "사용가능한 닉네임 입니다."
                   : `글자 수: ${length.nickname}/10`
