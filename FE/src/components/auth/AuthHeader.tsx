4014dfda521a3df131731b389727142b5fe84e06
dustnzlzl <dustnzlzl@naver.com>
2023-11-20 17:41:56 +0900

    [FE] modify: auth페이지 토글 버튼 제거

--- FE/src/components/auth/AuthHeader.tsx
+++ FE/src/components/auth/AuthHeader.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { ArrowBackIosNew } from "@mui/icons-material";
-import Toggle from "@components/common/Header/Toggle";
-import { Box, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 
 const AuthHeader = () => {
   const navigate = useNavigate();
@@ -12,11 +11,8 @@ const AuthHeader = () => {
   };
 
   return (
-    <Stack sx={{ position: "fixed", width: 1, mt: 1 }} direction="row" justifyContent="space-between">
-      <ArrowBackIosNew sx={{ pl: 3 }} fontSize="small" onClick={onClickBtn} />
-      <Box sx={{ pr: 3 }}>
-        <Toggle />
-      </Box>
+    <Stack sx={{ position: "fixed", width: 1, mt: 2, ml: 2 }} direction="row" justifyContent="space-between">
+      <ArrowBackIosNew sx={{ cursor: "pointer" }} fontSize="small" onClick={onClickBtn} />
     </Stack>
   );
 };
