845a3367ba1394b835883602afebc50e6d23d94d
taboowiths <tabou9820@gmail.com>
2023-11-21 15:13:07 +0900

    [FE] feat: 캘린더 소개글 작성 컴포넌트 생성

--- FE/src/components/planner/month/MonthDetailInfo/Introduction.tsx
+++ FE/src/components/planner/month/MonthDetailInfo/Introduction.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import styles from "@styles/planner/Month.module.scss";
+import Text from "@components/common/Text";
 
-const Introduction = () => {
-  return <div className={styles["introduction"]}>안녕하세요 ~</div>;
+interface Props {
+  isEdit: boolean;
+  setIsEdit: Dispatch<SetStateAction<boolean>>;
+}
+
+const Introduction = ({ isEdit, setIsEdit }: Props) => {
+  const [introduction, setIntroduction] = useState<string>("안녕하세요~");
+  return (
+    <div className={styles["introduction"]}>
+      {isEdit ? (
+        <div className={styles["introduction__edit"]}>
+          <textarea value={introduction} onChange={(e) => setIntroduction(e.target.value)} />
+        </div>
+      ) : (
+        <Text>{introduction}</Text>
+      )}
+    </div>
+  );
 };
 
 export default Introduction;
--- FE/src/components/planner/month/MonthIconBox.tsx
+++ FE/src/components/planner/month/MonthIconBox.tsx
@@ -1,4 +1,4 @@
-import React, { MutableRefObject, ReactNode } from "react";
+import React, { useState } from "react";
 import styles from "@styles/planner/Month.module.scss";
 import Text from "@components/common/Text";
 import Introduction from "@components/planner/month/MonthDetailInfo/Introduction";
@@ -10,6 +10,10 @@ import PeopleIcon from "@mui/icons-material/People";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import MessageIcon from "@mui/icons-material/Message";
 import EditIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
+import { useAppSelector } from "@hooks/hook";
+import { selectUserId } from "@store/authSlice";
+import { selectFriendId } from "@store/friendSlice";
 
 interface Props {
   types: "소개글" | "월별통계" | "팔로우" | "좋아요" | "방명록";
@@ -39,14 +43,26 @@ const IconType = {
 };
 
 const MonthIconBox = ({ types }: Props) => {
+  const userId = useAppSelector(selectUserId);
+  let friendId = useAppSelector(selectFriendId);
+  friendId = friendId != 0 ? friendId : userId;
+  const [isEdit, setIsEdit] = useState<boolean>(false);
   return (
     <div className={styles["icon-box"]}>
       <div className={styles["icon-box__title"]}>
-        <div style={{ backgroundColor: `${IconType[types].color}` }}>{IconType[types].icon}</div>
-        <Text>{types}</Text>
-        {types === "소개글" && <EditIcon />}
+        <div>
+          <div style={{ backgroundColor: `${IconType[types].color}` }}>{IconType[types].icon}</div>
+          <Text>{types}</Text>
+        </div>
+        {types === "소개글" &&
+          friendId === userId &&
+          (isEdit ? <SaveIcon onClick={() => setIsEdit(false)} /> : <EditIcon onClick={() => setIsEdit(true)} />)}
       </div>
-      {types === "소개글" ? <Introduction /> : <>{types === "방명록" ? <GuestBook /> : <Statistics types={types} />}</>}
+      {types === "소개글" ? (
+        <Introduction isEdit={isEdit} setIsEdit={setIsEdit} />
+      ) : (
+        <>{types === "방명록" ? <GuestBook /> : <Statistics types={types} />}</>
+      )}
     </div>
   );
 };
