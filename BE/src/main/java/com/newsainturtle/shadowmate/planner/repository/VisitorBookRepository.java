0177f951add6d07813804c3f6de0983932210bf9
yunaghgh <yunaghgh@naver.com>
2023-11-21 13:37:10 +0900

    [BE] feat: 방명록 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/VisitorBookRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/VisitorBookRepository.java
@@ -1,10 +1,15 @@
 package com.newsainturtle.shadowmate.planner.repository;
 
 import com.newsainturtle.shadowmate.planner.entity.VisitorBook;
+import com.newsainturtle.shadowmate.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VisitorBookRepository extends JpaRepository<VisitorBook, Long> {
     VisitorBook findByIdAndOwnerId(final long visitorBookId, final long ownerId);
+    List<VisitorBook> findTop5ByOwnerAndIdLessThanOrderByIdDesc(final User owner, final long lastVisitorBookId);
+    List<VisitorBook> findTop5ByOwnerOrderByIdDesc(final User owner);
 }
