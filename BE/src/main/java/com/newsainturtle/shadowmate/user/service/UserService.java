efb3fefdcf50dc7ee842b3b96c4d1322a383e35d
tgb02087 <tgb02087@naver.com>
2023-11-23 14:12:13 +0900

    [BE] feat: 소개글 조회 Service API 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/user/service/UserService.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/user/service/UserService.java
@@ -1,9 +1,6 @@
 package com.newsainturtle.shadowmate.user.service;
 
-import com.newsainturtle.shadowmate.user.dto.ProfileResponse;
-import com.newsainturtle.shadowmate.user.dto.UpdateIntroductionRequest;
-import com.newsainturtle.shadowmate.user.dto.UpdateUserRequest;
-import com.newsainturtle.shadowmate.user.dto.UserResponse;
+import com.newsainturtle.shadowmate.user.dto.*;
 import com.newsainturtle.shadowmate.user.entity.User;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +10,8 @@ public interface UserService {
     ProfileResponse getProfile(final Long userId);
 
     UserResponse searchNickname(final User user, final String nickname);
+
+    SearchIntroductionResponse searchIntroduction(final Long userId);
 
     void updateUser(final Long userId, final UpdateUserRequest updateUserRequest);
 
