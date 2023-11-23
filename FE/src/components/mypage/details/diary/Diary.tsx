bb5425c3efdb362e5633eff538d06bd694242c8e
taboowiths <tabou9820@gmail.com>
2023-11-23 11:13:20 +0900

    [FE] fix: 다이어리 공개 여부 변경 시 경고 모달 문구 수정

--- FE/src/components/mypage/details/diary/Diary.tsx
+++ FE/src/components/mypage/details/diary/Diary.tsx
@@ -86,7 +86,7 @@ const MyPageDiary = () => {
           <Text types="small">
             <>전체 공개 및 친구 공개 상태에서 비공개 전환 시,</>
             <br />
-            <>소셜에 공유된 게시글이 삭제됩니다.</>
+            <>소셜에 공유된 게시글이 숨김 처리됩니다.</>
           </Text>
           <Text types="small">계속 진행하시겠습니까?</Text>
         </div>
