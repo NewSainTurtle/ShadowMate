47f1fde1ef577a9a1f0b2da23db31ab1ae7c2138
dustnzlzl <dustnzlzl@naver.com>
2023-11-27 23:55:27 +0900

    [FE] modify: 이미지 리사이징 화질 개선

--- FE/src/util/resizeImage.ts
+++ FE/src/util/resizeImage.ts
@@ -7,10 +7,10 @@ const dataURItoBlob = function (dataURI: string) {
   return new Blob([ia], { type: mimeString });
 };
 
-export const resizeImage = (file: File): Promise<Blob> => {
+export const resizeImage = (file: File | Blob): Promise<Blob> => {
   const canvas = document.createElement("canvas");
   const base_size = 256000; //250KB // 파일 압축 기준 사이즈
-  const comp_size = 51200; //50KB (25~100KB 수준으로 압축)
+  const comp_size = 51200; //50KB
 
   return new Promise((resolve, reject) => {
     const image = new Image();
@@ -20,7 +20,7 @@ export const resizeImage = (file: File): Promise<Blob> => {
       let height = image.height;
       if (file.size <= base_size) return resolve(file);
 
-      const ratio = Math.ceil(file.size / comp_size);
+      const ratio = Math.ceil(Math.sqrt(file.size / comp_size));
       width = image.width / ratio;
       height = image.height / ratio;
       canvas.width = width;
