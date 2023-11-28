f4dbf0d2049b0fcecf41c0524d9cc7ceffa6344b
tgb02087 <tgb02087@naver.com>
2023-11-28 15:03:55 +0900

    [BE] refactor: 회원삭제시 팔로우삭제 followRequest Repository API 리팩토링

--- BE/src/main/java/com/newsainturtle/shadowmate/follow/repository/FollowRequestRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/follow/repository/FollowRequestRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +16,11 @@ public interface FollowRequestRepository extends JpaRepository<FollowRequest, Lo
     List<FollowRequest> findAllByReceiverId(final User receiver);
 
     void deleteByRequesterIdAndReceiverId(final User requester, final User receiver);
+
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM FollowRequest f WHERE f.requesterId = :requesterId OR f.receiverId = :receiverId")
+    void deleteAllByRequesterIdOrReceiverId(@Param("requesterId") final User requesterId, @Param("receiverId") final User receiverId);
+
 
     @Modifying(clearAutomatically = true)
     @Query("delete from FollowRequest f where f.receiverId.id = :receiverId")
