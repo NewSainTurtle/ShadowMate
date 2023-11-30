a47fc7754828d1e6f89eb970286c1c498987918e
taboowiths <tabou9820@gmail.com>
2023-11-30 15:21:40 +0900

    [FE] design: 화면 여백 설정

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "src/App";
 import { BrowserRouter } from "react-router-dom";
 import { StyledEngineProvider } from "@mui/material/styles";
 import { persistor, store } from "@hooks/configStore";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import "src/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
