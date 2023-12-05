766a56732a9d3462d2453cc50a8c6e52d9bd8d7e
dustnzlzl <dustnzlzl@naver.com>
2023-12-06 04:16:04 +0900

    [FE] feat: 회원가입 동의 기능 추가

--- FE/src/components/auth/signup/Signup.tsx
+++ FE/src/components/auth/signup/Signup.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "@styles/auth/Signup.module.scss";
 import SignupAccount from "./SignupAccount";
+import SignupContract from "./SignupContract";
 import Text from "@components/common/Text";
 
 const Signup = () => {
+  const [isNext, setNext] = useState(false);
+  const handleClickNext = () => setNext(true);
+
   return (
     <div className={styles["signup_display"]}>
       <div className={styles["signup_container"]}>
         <div className={styles["signup_logo"]}>Sign up</div>
-
-        <SignupAccount />
-
+        {!isNext ? <SignupContract handleClickNext={handleClickNext} /> : <SignupAccount />}
         <div className={styles["signup_context"]}>
           <Text types="small">이미 회원이신가요?</Text>
           <NavLink to={"/login"}>
--- FE/src/components/auth/signup/SignupContract.tsx
+++ FE/src/components/auth/signup/SignupContract.tsx
@@ -1,19 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, ChangeEvent } from "react";
 import styles from "@styles/auth/Signup.module.scss";
 import AuthButton from "../AuthButton";
 import Text from "@components/common/Text";
 import Checkbox from "@components/common/Checkbox";
 import { Conditions, PersonalInformation } from "@util/Consent";
 
-const SignupContract = () => {
-  const onClickNext = () => {};
+const SignupContract = ({ handleClickNext }: { handleClickNext: () => void }) => {
+  const [allAgreed, setAllAgreed] = useState(false);
+  const [agreements, setAgreements] = useState({
+    conditionsAgreed: false,
+    personalInfoAgreed: false,
+  });
+
+  const handleAgreementChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    setAgreements({ ...agreements, [name]: checked });
+    const allChecked = Object.values({ ...agreements, [name]: checked }).every((value) => value == true);
+    setAllAgreed(allChecked);
+  };
+
+  const handleAllAgreementChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+    setAgreements({
+      ...agreements,
+      conditionsAgreed: checked,
+      personalInfoAgreed: checked,
+    });
+    setAllAgreed(checked);
+  };
+
+  const onClickNext = () => {
+    if (allAgreed) handleClickNext();
+  };
+
+  const { conditionsAgreed, personalInfoAgreed } = agreements;
 
   return (
     <>
       <div className={styles["signup-contract__form"]}>
         <div className={`${styles["signup-contract__line"]} ${styles["all-agree"]}`}>
-          <Text types="small">전체 동의 합니다.</Text>
-          <Checkbox />
+          <Text types="small">전체 동의하기</Text>
+          <Checkbox name="allAgreed" checked={allAgreed} onChange={handleAllAgreementChange} />
         </div>
 
         <div>
@@ -22,7 +49,7 @@ const SignupContract = () => {
               <Text types="small">이용약관 동의</Text>
               <Text types="small"> (필수)</Text>
             </div>
-            <Checkbox />
+            <Checkbox name="conditionsAgreed" checked={conditionsAgreed} onChange={handleAgreementChange} />
           </div>
           <textarea className={styles["signup-contract__textarea"]} readOnly>
             {Conditions}
@@ -35,7 +62,7 @@ const SignupContract = () => {
               <Text types="small">개인정보 수집 및 이용 동의</Text>
               <Text types="small"> (필수)</Text>
             </div>
-            <Checkbox />
+            <Checkbox name="personalInfoAgreed" checked={personalInfoAgreed} onChange={handleAgreementChange} />
           </div>
           <textarea className={styles["signup-contract__textarea"]} readOnly>
             {PersonalInformation}
@@ -43,8 +70,8 @@ const SignupContract = () => {
         </div>
       </div>
 
-      <div className={styles["signup-contract__button"]} onClick={onClickNext}>
-        <AuthButton>Next</AuthButton>
+      <div className={`${styles["signup-contract__button"]} ${!allAgreed ? styles["--disable"] : ""}`}>
+        <AuthButton onClick={onClickNext}>Next</AuthButton>
       </div>
     </>
   );
