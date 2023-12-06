712285de62ed2a14448f51eadb30ad6a5a00c0f4
taboowiths <tabou9820@gmail.com>
2023-12-06 14:39:01 +0900

    [FE] feat: 팔로우 리스트 state 생성

--- FE/src/store/friendSlice.ts
+++ FE/src/store/friendSlice.ts
@@ -1,12 +1,14 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { rootState } from "@hooks/configStore";
 import { ProfileConfig } from "@components/common/FriendProfile";
+import { followingType } from "@util/friend.interface";
 import dayjs from "dayjs";
 
 interface friendConfig {
   followState: number;
   friendDate: string;
   friendInfo: ProfileConfig;
+  followingList: followingType[];
 }
 
 const initialState: friendConfig = {
@@ -18,6 +20,7 @@ const initialState: friendConfig = {
     profileImage: "",
     statusMessage: "",
   },
+  followingList: [],
 };
 
 const friendSlice = createSlice({
@@ -34,15 +37,24 @@ const friendSlice = createSlice({
     setFollowState: (state, { payload }: PayloadAction<friendConfig["followState"]>) => {
       state.followState = payload;
     },
+    setFollowingList: (state, { payload }: PayloadAction<followingType[]>) => {
+      console.log(payload);
+      state.followingList = payload;
+    },
     clearFriendInfo: (state) => {
       state.friendInfo = initialState.friendInfo;
+    },
+    clearFollowingList: (state) => {
+      state.followingList = initialState.followingList;
     },
   },
 });
 
-export const { setFriendDate, setFriendInfo, setFollowState, clearFriendInfo } = friendSlice.actions;
+export const { setFriendDate, setFriendInfo, setFollowState, clearFriendInfo, setFollowingList, clearFollowingList } =
+  friendSlice.actions;
 export const selectFriendDate = (state: rootState) => state.friend.friendDate;
 export const selectFriendInfo = (state: rootState) => state.friend.friendInfo;
 export const selectFriendId = (state: rootState) => state.friend.friendInfo.userId;
 export const selectFollowState = (state: rootState) => state.friend.followState;
+export const selectFollowingList = (state: rootState) => state.friend.followingList;
 export default friendSlice.reducer;
