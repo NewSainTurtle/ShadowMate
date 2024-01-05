5b84362718fb31e5b1c96d416566b40f58fe27f2
yunaghgh <yunaghgh@naver.com>
2024-01-05 21:52:27 +0900

    [BE] feat: 소셜 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/social/repository/SocialRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/social/repository/SocialRepository.java
@@ -2,6 +2,7 @@ package com.newsainturtle.shadowmate.social.repository;
 
 import com.newsainturtle.shadowmate.planner.entity.DailyPlanner;
 import com.newsainturtle.shadowmate.social.entity.Social;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,8 +15,21 @@ import java.util.List;
 @Repository
 public interface SocialRepository extends JpaRepository<Social, Long> {
 
-    @Query(value = "SELECT s FROM Social s WHERE s.deleteTime is null")
-    List<Social> findAllByDeleteTime();
+    @Query("SELECT s FROM Social s where s.deleteTime is null order by s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByDeleteTimeIsNullSortLatest(final Pageable pageable);
+
+    @Query("SELECT s FROM Social s LEFT JOIN fetch DailyPlannerLike l on s.dailyPlanner.id = l.dailyPlanner.id where s.deleteTime is null GROUP BY s.id order by COUNT(l.dailyPlanner.id) desc, s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByDeleteTimeIsNullSortPopularity(final Pageable pageable);
+
+    Integer countByDeleteTimeIsNull();
+
+    @Query("SELECT s FROM Social s where s.ownerId = :id and s.deleteTime is null order by s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByOwnerIdAndDeleteTimeIsNullSortLatest(@Param("id") final long id, final Pageable pageable);
+
+    @Query("SELECT s FROM Social s LEFT JOIN fetch DailyPlannerLike l on s.dailyPlanner.id = l.dailyPlanner.id where s.ownerId = :id and s.deleteTime is null GROUP BY s.id order by COUNT(l.dailyPlanner.id) desc, s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByOwnerIdAndDeleteTimeIsNullSortPopularity(@Param("id") final long id, final Pageable pageable);
+
+    Integer countByOwnerIdAndDeleteTimeIsNull(final long ownerId);
 
     Social findByDailyPlanner(final DailyPlanner dailyPlanner);
 
@@ -25,6 +39,4 @@ public interface SocialRepository extends JpaRepository<Social, Long> {
     @Query("update Social s set s.deleteTime = :time where s.dailyPlanner in (:dailyPlanners)")
     void updateDeleteTimeAll(@Param("time") final LocalDateTime time, @Param("dailyPlanners") final List<DailyPlanner> dailyPlanners);
 
-    @Query("SELECT DISTINCT s FROM Social s LEFT JOIN fetch DailyPlanner d on s.dailyPlanner.id = d.id WHERE s.deleteTime is null and d.user.id = :id")
-    List<Social> findAllByDailyPlannerAndSocial(@Param("id") final long id);
 }
