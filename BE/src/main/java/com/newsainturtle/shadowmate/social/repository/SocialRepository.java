3767677a242349056a4df14d6509d9d948421c1b
yunaghgh <yunaghgh@naver.com>
2024-01-06 00:24:28 +0900

    [BE] feat: 기간 필터 설정하여 소셜 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/social/repository/SocialRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/social/repository/SocialRepository.java
@@ -15,22 +15,6 @@ import java.util.List;
 @Repository
 public interface SocialRepository extends JpaRepository<Social, Long> {
 
-    @Query("SELECT s FROM Social s where s.deleteTime is null order by s.dailyPlannerDay desc, s.createTime desc")
-    List<Social> findAllByDeleteTimeIsNullSortLatest(final Pageable pageable);
-
-    @Query("SELECT s FROM Social s LEFT JOIN fetch DailyPlannerLike l on s.dailyPlanner.id = l.dailyPlanner.id where s.deleteTime is null GROUP BY s.id order by COUNT(l.dailyPlanner.id) desc, s.dailyPlannerDay desc, s.createTime desc")
-    List<Social> findAllByDeleteTimeIsNullSortPopularity(final Pageable pageable);
-
-    Integer countByDeleteTimeIsNull();
-
-    @Query("SELECT s FROM Social s where s.ownerId = :id and s.deleteTime is null order by s.dailyPlannerDay desc, s.createTime desc")
-    List<Social> findAllByOwnerIdAndDeleteTimeIsNullSortLatest(@Param("id") final long id, final Pageable pageable);
-
-    @Query("SELECT s FROM Social s LEFT JOIN fetch DailyPlannerLike l on s.dailyPlanner.id = l.dailyPlanner.id where s.ownerId = :id and s.deleteTime is null GROUP BY s.id order by COUNT(l.dailyPlanner.id) desc, s.dailyPlannerDay desc, s.createTime desc")
-    List<Social> findAllByOwnerIdAndDeleteTimeIsNullSortPopularity(@Param("id") final long id, final Pageable pageable);
-
-    Integer countByOwnerIdAndDeleteTimeIsNull(final long ownerId);
-
     Social findByDailyPlanner(final DailyPlanner dailyPlanner);
 
     Social findByDailyPlannerAndDeleteTimeIsNull(final DailyPlanner dailyPlanner);
@@ -39,4 +23,41 @@ public interface SocialRepository extends JpaRepository<Social, Long> {
     @Query("update Social s set s.deleteTime = :time where s.dailyPlanner in (:dailyPlanners)")
     void updateDeleteTimeAll(@Param("time") final LocalDateTime time, @Param("dailyPlanners") final List<DailyPlanner> dailyPlanners);
 
+    /** 일반 소셜 조회 **/
+    @Query("SELECT s FROM Social s where s.deleteTime is null order by s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByDeleteTimeIsNullSortLatest(final Pageable pageable);
+
+    @Query("SELECT s FROM Social s LEFT JOIN fetch DailyPlannerLike l on s.dailyPlanner.id = l.dailyPlanner.id where s.deleteTime is null GROUP BY s.id order by COUNT(l.dailyPlanner.id) desc, s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByDeleteTimeIsNullSortPopularity(final Pageable pageable);
+
+    Integer countByDeleteTimeIsNull();
+
+    /** 닉네임 검색 소셜 조회 **/
+    @Query("SELECT s FROM Social s where s.ownerId = :id and s.deleteTime is null order by s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByOwnerIdAndDeleteTimeIsNullSortLatest(@Param("id") final long id, final Pageable pageable);
+
+    @Query("SELECT s FROM Social s LEFT JOIN fetch DailyPlannerLike l on s.dailyPlanner.id = l.dailyPlanner.id where s.ownerId = :id and s.deleteTime is null GROUP BY s.id order by COUNT(l.dailyPlanner.id) desc, s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByOwnerIdAndDeleteTimeIsNullSortPopularity(@Param("id") final long id, final Pageable pageable);
+
+    Integer countByOwnerIdAndDeleteTimeIsNull(final long ownerId);
+
+    /** 일반 소셜 기간 조회 **/
+    @Query("SELECT s FROM Social s where s.deleteTime is null and s.dailyPlannerDay >= :startDate and s.dailyPlannerDay <= :endDate order by s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByDeleteTimeIsNullAndPeriodSortLatest(@Param("startDate") final String startDate, @Param("endDate") final String endDate, final Pageable pageable);
+
+    @Query("SELECT s FROM Social s LEFT JOIN fetch DailyPlannerLike l on s.dailyPlanner.id = l.dailyPlanner.id where s.deleteTime is null and s.dailyPlannerDay >= :startDate and s.dailyPlannerDay <= :endDate GROUP BY s.id order by COUNT(l.dailyPlanner.id) desc, s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByDeleteTimeIsNullAndPeriodSortPopularity(@Param("startDate") final String startDate, @Param("endDate") final String endDate, final Pageable pageable);
+
+    @Query("SELECT count(s) FROM Social s where s.deleteTime is null and s.dailyPlannerDay >= :startDate and s.dailyPlannerDay <= :endDate")
+    Integer countByDeleteTimeIsNullAndPeriod(@Param("startDate") final String startDate, @Param("endDate") final String endDate);
+
+    /** 닉네임 검색 소셜 기간 조회 **/
+    @Query("SELECT s FROM Social s where s.ownerId = :id and s.deleteTime is null and s.dailyPlannerDay >= :startDate and s.dailyPlannerDay <= :endDate order by s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByOwnerIdAndDeleteTimeIsNullAndPeriodSortLatest(@Param("id") final long id, @Param("startDate") final String startDate, @Param("endDate") final String endDate, final Pageable pageable);
+
+    @Query("SELECT s FROM Social s LEFT JOIN fetch DailyPlannerLike l on s.dailyPlanner.id = l.dailyPlanner.id where s.ownerId = :id and s.deleteTime is null and s.dailyPlannerDay >= :startDate and s.dailyPlannerDay <= :endDate GROUP BY s.id order by COUNT(l.dailyPlanner.id) desc, s.dailyPlannerDay desc, s.createTime desc")
+    List<Social> findAllByOwnerIdAndDeleteTimeIsNullAndPeriodSortPopularity(@Param("id") final long id, @Param("startDate") final String startDate, @Param("endDate") final String endDate, final Pageable pageable);
+
+    @Query("SELECT count(s) FROM Social s where s.ownerId = :id and s.deleteTime is null and s.dailyPlannerDay >= :startDate and s.dailyPlannerDay <= :endDate")
+    Integer countByOwnerIdAndDeleteTimeIsNullAndPeriod(@Param("id") final long id, @Param("startDate") final String startDate, @Param("endDate") final String endDate);
 }
