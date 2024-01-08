e36d941612e482f71bfb2a27ba359bea54e43b07
yunaghgh <yunaghgh@naver.com>
2024-01-08 15:37:02 +0900

    [BE] feat: 타임테이블 삭제와 목록조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/TimeTableRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner/repository/TimeTableRepository.java
@@ -1,9 +1,14 @@
 package com.newsainturtle.shadowmate.planner.repository;
 
 import com.newsainturtle.shadowmate.planner.entity.TimeTable;
+import com.newsainturtle.shadowmate.planner.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TimeTableRepository extends JpaRepository<TimeTable, Long> {
+    void deleteByIdAndTodoId(final Long id, final Long todoId);
+    List<TimeTable> findAllByTodo(final Todo todo);
 }
