a504c2ce86893edf28aacd04782ba52de7bc8a1d
taboowiths <tabou9820@gmail.com>
2024-01-09 16:27:32 +0900

    [FE] refactor: 불필요한 코드 삭제

--- FE/src/components/common/Profile/index.tsx
+++ FE/src/components/common/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@styles/common/Profile.module.scss";
 import Text from "@components/common/Text";
 import Button from "@components/common/Button";
