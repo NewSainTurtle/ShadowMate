b3d1a0557ff069ef3db0c2d2ab320c95fc483fe5
yunaghgh <yunaghgh@naver.com>
2024-01-15 22:47:10 +0900

    [BE] test: 루틴 등록 레포지토리 테스트 코드 작성

---
+++ BE/src/test/java/com/newsainturtle/shadowmate/yn/planner_setting/repository/RoutineRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.newsainturtle.shadowmate.yn.planner_setting.repository;
+
+import com.newsainturtle.shadowmate.planner_setting.entity.Routine;
+import com.newsainturtle.shadowmate.planner_setting.repository.RoutineRepository;
+import com.newsainturtle.shadowmate.user.entity.User;
+import com.newsainturtle.shadowmate.user.enums.PlannerAccessScope;
+import com.newsainturtle.shadowmate.user.enums.SocialType;
+import com.newsainturtle.shadowmate.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class RoutineRepositoryTest {
+
+    @Autowired
+    private RoutineRepository routineRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+    private final String startDay = "2023-12-25";
+    private final String endDay = "2024-01-05";
+    private final String routineContent = "아침운동하기";
+
+    @BeforeEach
+    void init() {
+        user = userRepository.save(User.builder()
+                .email("yntest@shadowmate.com")
+                .password("yntest1234")
+                .socialLogin(SocialType.BASIC)
+                .nickname("거북이")
+                .plannerAccessScope(PlannerAccessScope.PUBLIC)
+                .withdrawal(false)
+                .build());
+    }
+
+    @Test
+    void 루틴등록() {
+        //given
+        final Routine routine = Routine.builder()
+                .startDay(startDay)
+                .endDay(endDay)
+                .routineContent(routineContent)
+                .category(null)
+                .user(user)
+                .build();
+
+        //when
+        final Routine saveRoutine = routineRepository.save(routine);
+
+        //then
+        assertThat(saveRoutine).isNotNull();
+        assertThat(saveRoutine.getStartDay()).isEqualTo(startDay);
+        assertThat(saveRoutine.getEndDay()).isEqualTo(endDay);
+        assertThat(saveRoutine.getRoutineContent()).isEqualTo(routineContent);
+    }
+
+}
