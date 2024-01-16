7307b48a9e09cd482f179a92199a5a3065cc4447
taboowiths <tabou9820@gmail.com>
2024-01-16 22:28:53 +0900

    [FE] fix: 마이페이지 루틴 카테고리명 에러 수정

--- FE/src/components/mypage/details/diary/Routine.tsx
+++ FE/src/components/mypage/details/diary/Routine.tsx
@@ -75,9 +75,10 @@ const Routine = () => {
           <Text>카테고리</Text>
           <Input
             name="routineCategory"
-            value={category.categoryTitle}
+            value={category?.categoryId === 0 ? "카테고리 없음" : category.categoryTitle}
             onClick={handleOpen}
             style={{ caretColor: "transparent" }}
+            placeholder="루틴의 카테고리를 선택하세요."
           />
         </div>
 
