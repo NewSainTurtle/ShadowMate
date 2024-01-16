da55f34df64c30097ef986002e5f9d12b2d4de90
yunaghgh <yunaghgh@naver.com>
2024-01-16 18:14:21 +0900

    [BE] feat: 루틴 할일 목록 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/RoutineTodoRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/RoutineTodoRepository.java
@@ -1,5 +1,6 @@
 package com.newsainturtle.shadowmate.planner_setting.repository;
 
+import com.newsainturtle.shadowmate.planner_setting.entity.Routine;
 import com.newsainturtle.shadowmate.planner_setting.entity.RoutineTodo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,9 +10,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoutineTodoRepository extends JpaRepository<RoutineTodo, Long> {
 
+    RoutineTodo[] findAllByRoutineAndTodoIsNull(final Routine routine);
+    RoutineTodo[] findAllByRoutineAndTodoIsNullAndDailyPlannerDayLessThanEqual(final Routine routine, final String dailyPlannerDay);
+    RoutineTodo[] findAllByRoutineAndTodoIsNotNull(final Routine routine);
+    RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDailyPlannerDayAfter(final Routine routine, final String dailyPlannerDay);
+
     @Query("SELECT rt FROM RoutineTodo rt LEFT JOIN fetch Routine r on rt.routine.id = r.id where r.user.id = :userId and rt.dailyPlannerDay = :dailyPlannerDay and rt.todo is null")
     RoutineTodo[] findAllByUserAndDailyPlannerDayAndTodoIsNull(@Param("userId") final Long userId, @Param("dailyPlannerDay") final String dailyPlannerDay);
 
     @Query("SELECT count(rt) FROM RoutineTodo rt LEFT JOIN fetch Routine r on rt.routine.id = r.id where r.user.id = :userId and rt.dailyPlannerDay = :dailyPlannerDay and rt.todo is null")
     int countByUserAndDailyPlannerDayAndTodoIsNull(@Param("userId") final Long userId, @Param("dailyPlannerDay") final String dailyPlannerDay);
-}
+
+    }
