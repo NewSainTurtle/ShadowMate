57eab7ddfbbffd8b3e1c41f0516c9890f089a615
yunaghgh <yunaghgh@naver.com>
2024-01-17 15:54:06 +0900

    [BE] feat: 해당 카테고리가 있는 루틴 카운트 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/RoutineRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/RoutineRepository.java
@@ -1,5 +1,6 @@
 package com.newsainturtle.shadowmate.planner_setting.repository;
 
+import com.newsainturtle.shadowmate.planner_setting.entity.Category;
 import com.newsainturtle.shadowmate.planner_setting.entity.Routine;
 import com.newsainturtle.shadowmate.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ public interface RoutineRepository extends JpaRepository<Routine, Long> {
     Routine[] findAllByUser(final User user);
     Routine findByIdAndUser(final Long id, final User user);
     void deleteByIdAndUser(final Long id, final User user);
+    long countByCategory(final Category category);
 }
