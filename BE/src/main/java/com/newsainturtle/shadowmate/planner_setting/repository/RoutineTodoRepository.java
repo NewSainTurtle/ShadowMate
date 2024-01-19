e8cc76e0b1bd13bcb549d17742eeb3eb40f14323
yunaghgh <yunaghgh@naver.com>
2024-01-19 23:33:36 +0900

    [BE] feat: 루틴 목록 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/RoutineTodoRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/RoutineTodoRepository.java
@@ -7,13 +7,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoutineTodoRepository extends JpaRepository<RoutineTodo, Long> {
 
     RoutineTodo[] findAllByRoutineAndTodoIsNull(final Routine routine);
+    RoutineTodo[] findAllByRoutineAndTodoIsNullAndDay(final Routine routine, final String day);
     RoutineTodo[] findAllByRoutineAndTodoIsNullAndDailyPlannerDayLessThan(final Routine routine, final String dailyPlannerDay);
+    RoutineTodo[] findAllByRoutineAndTodoIsNullAndDailyPlannerDayBetweenAndDayIn(final Routine routine, final String startDay, final String endDay, final List<String> days);
     RoutineTodo[] findAllByRoutineAndTodoIsNotNull(final Routine routine);
+    RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDay(final Routine routine, final String day);
+    RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDayIn(final Routine routine, final List<String> days);
     RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDailyPlannerDayGreaterThanEqual(final Routine routine, final String dailyPlannerDay);
+    RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDailyPlannerDayBetweenAndDayIn(final Routine routine, final String startDay, final String endDay, final List<String> days);
 
     @Query("SELECT rt FROM RoutineTodo rt LEFT JOIN fetch Routine r on rt.routine.id = r.id where r.user.id = :userId and rt.dailyPlannerDay = :dailyPlannerDay and rt.todo is null")
     RoutineTodo[] findAllByUserAndDailyPlannerDayAndTodoIsNull(@Param("userId") final Long userId, @Param("dailyPlannerDay") final String dailyPlannerDay);
@@ -21,4 +28,4 @@ public interface RoutineTodoRepository extends JpaRepository<RoutineTodo, Long> 
     @Query("SELECT count(rt) FROM RoutineTodo rt LEFT JOIN fetch Routine r on rt.routine.id = r.id where r.user.id = :userId and rt.dailyPlannerDay = :dailyPlannerDay and rt.todo is null")
     int countByUserAndDailyPlannerDayAndTodoIsNull(@Param("userId") final Long userId, @Param("dailyPlannerDay") final String dailyPlannerDay);
 
-}
+    }
