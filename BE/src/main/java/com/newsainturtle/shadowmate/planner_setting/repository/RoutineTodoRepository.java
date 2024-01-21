bb121aaa2dafdeba1acc455bce8208c14dda0277
yunaghgh <yunaghgh@naver.com>
2024-01-21 23:52:08 +0900

    [BE] feat: 루틴 목록 조회 레포지토리 프로덕션 코드 작성

--- BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/RoutineTodoRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/planner_setting/repository/RoutineTodoRepository.java
@@ -12,14 +12,19 @@ import java.util.List;
 @Repository
 public interface RoutineTodoRepository extends JpaRepository<RoutineTodo, Long> {
 
+    RoutineTodo[] findAllByRoutineAndDailyPlannerDayLessThanAndDay(final Routine routine, final String dailyPlannerDay, final String day);
     RoutineTodo[] findAllByRoutineAndTodoIsNull(final Routine routine);
     RoutineTodo[] findAllByRoutineAndTodoIsNullAndDay(final Routine routine, final String day);
     RoutineTodo[] findAllByRoutineAndTodoIsNullAndDailyPlannerDayLessThan(final Routine routine, final String dailyPlannerDay);
+    RoutineTodo[] findAllByRoutineAndTodoIsNullAndDailyPlannerDayLessThanAndDayIn(final Routine routine, final String dailyPlannerDay, final List<String> days);
     RoutineTodo[] findAllByRoutineAndTodoIsNullAndDailyPlannerDayBetweenAndDayIn(final Routine routine, final String startDay, final String endDay, final List<String> days);
+    RoutineTodo[] findAllByRoutineAndTodoIsNullAndDailyPlannerDayGreaterThanEqualAndDay(final Routine routine, final String dailyPlannerDay, final String day);
     RoutineTodo[] findAllByRoutineAndTodoIsNotNull(final Routine routine);
     RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDay(final Routine routine, final String day);
     RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDayIn(final Routine routine, final List<String> days);
     RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDailyPlannerDayGreaterThanEqual(final Routine routine, final String dailyPlannerDay);
+    RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDailyPlannerDayGreaterThanEqualAndDay(final Routine routine, final String dailyPlannerDay, final String day);
+    RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDailyPlannerDayGreaterThanEqualAndDayIn(final Routine routine, final String dailyPlannerDay, final List<String> days);
     RoutineTodo[] findAllByRoutineAndTodoIsNotNullAndDailyPlannerDayBetweenAndDayIn(final Routine routine, final String startDay, final String endDay, final List<String> days);
 
     @Query("SELECT rt FROM RoutineTodo rt LEFT JOIN fetch Routine r on rt.routine.id = r.id where r.user.id = :userId and rt.dailyPlannerDay = :dailyPlannerDay and rt.todo is null")
