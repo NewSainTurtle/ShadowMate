7d4f9ad5212ecad9c32ae7bbfe011b2bb3ad4f42
yunaghgh <yunaghgh@naver.com>
2024-01-24 15:14:31 +0900

    [BE] refactor: 상수클래스 기본 생성자 비공개 처리

--- BE/src/main/java/com/newsainturtle/shadowmate/common/constant/CommonConstant.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/common/constant/CommonConstant.java
@@ -1,5 +1,9 @@
 package com.newsainturtle.shadowmate.common.constant;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommonConstant {
     public static final String DATE_PATTERN = "^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$";
     public static final String DATE_PATTERN_ERROR_MESSAGE = "yyyy-MM-dd 형식의 올바른 날짜값이 아닙니다.";
