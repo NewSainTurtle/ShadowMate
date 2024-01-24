1eae60ec8188d09c4be0aac3e34d620d1edb9825
yunaghgh <yunaghgh@naver.com>
2024-01-24 16:00:16 +0900

    [BE] refactor: 사용하지 않는 라이브러리 제거

--- BE/src/main/java/com/newsainturtle/shadowmate/social/repository/SocialRepository.java
+++ BE/src/main/java/com/newsainturtle/shadowmate/social/repository/SocialRepository.java
@@ -16,13 +16,13 @@ import java.util.List;
 public interface SocialRepository extends JpaRepository<Social, Long> {
 
     Social findByDailyPlanner(final DailyPlanner dailyPlanner);
-
     Social findByDailyPlannerAndDeleteTimeIsNull(final DailyPlanner dailyPlanner);
 
     @Modifying(clearAutomatically = true)
     @Query("update Social s set s.deleteTime = :time where s.dailyPlanner in (:dailyPlanners)")
     void updateDeleteTimeAll(@Param("time") final LocalDateTime time, @Param("dailyPlanners") final List<DailyPlanner> dailyPlanners);
 
+    
     /** 일반 소셜 조회 **/
     @Query("SELECT s FROM Social s where s.deleteTime is null order by s.dailyPlannerDay desc, s.createTime desc")
     List<Social> findAllByDeleteTimeIsNullSortLatest(final Pageable pageable);
@@ -31,6 +31,7 @@ public interface SocialRepository extends JpaRepository<Social, Long> {
     List<Social> findAllByDeleteTimeIsNullSortPopularity(final Pageable pageable);
 
     Integer countByDeleteTimeIsNull();
+
 
     /** 닉네임 검색 소셜 조회 **/
     @Query("SELECT s FROM Social s where s.ownerId = :id and s.deleteTime is null order by s.dailyPlannerDay desc, s.createTime desc")
@@ -41,6 +42,7 @@ public interface SocialRepository extends JpaRepository<Social, Long> {
 
     Integer countByOwnerIdAndDeleteTimeIsNull(final long ownerId);
 
+
     /** 일반 소셜 기간 조회 **/
     @Query("SELECT s FROM Social s where s.deleteTime is null and s.dailyPlannerDay >= :startDate and s.dailyPlannerDay <= :endDate order by s.dailyPlannerDay desc, s.createTime desc")
     List<Social> findAllByDeleteTimeIsNullAndPeriodSortLatest(@Param("startDate") final String startDate, @Param("endDate") final String endDate, final Pageable pageable);
@@ -50,6 +52,7 @@ public interface SocialRepository extends JpaRepository<Social, Long> {
 
     @Query("SELECT count(s) FROM Social s where s.deleteTime is null and s.dailyPlannerDay >= :startDate and s.dailyPlannerDay <= :endDate")
     Integer countByDeleteTimeIsNullAndPeriod(@Param("startDate") final String startDate, @Param("endDate") final String endDate);
+
 
     /** 닉네임 검색 소셜 기간 조회 **/
     @Query("SELECT s FROM Social s where s.ownerId = :id and s.deleteTime is null and s.dailyPlannerDay >= :startDate and s.dailyPlannerDay <= :endDate order by s.dailyPlannerDay desc, s.createTime desc")
